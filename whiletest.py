898c3dbf0f62e2d666cb8b772ea336a1f55e9cef
dwashington102 <dwashington102@gmail.com>
2016-09-07 11:39:34 -0500

    Add files via upload

--- whiletest.py
+++ whiletest.py
@@ -4,7 +4,9 @@
 # Script will prompt user for a number.
 # Using the value provided by the user - the script loops through printing 10 lines. 
 
-iUser=int(input("Insert number here: "))
+iUser=int(input("Insert number here: ")) # User Input statement
+
+# Define variables
 x=(iUser)
 y=int(x+10)
 loop=1
@@ -15,7 +17,7 @@ while (x<=y):
 		str_y=str(y)
 		str_loop=str(loop)
 		#print("Line Count: ", loop ,' X Value: ',x,' Y Value',y,'Only 4 more lines')
-		print("Line Count: "+ str_loop +" X Value: " + str_x + " Y Value: " + str_y)
+		print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "] ONLY 4 MORE LINES")
 		x+=1
 		loop+=1
 	elif x!=y:
@@ -23,7 +25,7 @@ while (x<=y):
 		str_y=str(y)
 		str_loop=str(loop)
 		#print('Line Count: '+ str_loop + ' X Value: ',x,' Y Value',y)
-		print("Line Count: "+ str_loop +" X Value: " + str_x + " Y Value: " + str_y)
+		print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "]")
 		x+=1
 		loop+=1
 	else:
