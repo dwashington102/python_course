378dbf5683900e4aed2966a82cf0973ec5345363
dwashington102 <dwashington102@gmail.com>
2016-09-07 11:36:45 -0500

    Add files via upload

--- whiletest.py
+++ whiletest.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python
+# Date: 9/7/2016
+#
+# Script will prompt user for a number.
+# Using the value provided by the user - the script loops through printing 10 lines. 
 
 iUser=int(input("Insert number here: "))
 x=(iUser)
 y=int(x+10)
+loop=1
 
 while (x<=y):
-	loop=x+1
 	if x==y-5:
-		print('Only 4 more to go:  LoopCount: ',loop,' X Value: ',x,' Y Value',y)
+		str_x=str(x)
+		str_y=str(y)
+		str_loop=str(loop)
+		#print("Line Count: ", loop ,' X Value: ',x,' Y Value',y,'Only 4 more lines')
+		print("Line Count: "+ str_loop +" X Value: " + str_x + " Y Value: " + str_y)
 		x+=1
+		loop+=1
 	elif x!=y:
-		print('LoopCount: ',loop,' X Value: ',x,' Y Value',y)
+		str_x=str(x)
+		str_y=str(y)
+		str_loop=str(loop)
+		#print('Line Count: '+ str_loop + ' X Value: ',x,' Y Value',y)
+		print("Line Count: "+ str_loop +" X Value: " + str_x + " Y Value: " + str_y)
 		x+=1
+		loop+=1
 	else:
 		print('Thats all folks')
 		exit(0)
