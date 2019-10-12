67962e83d2ed825d3cb1eb2a22d7d33c1251ff13
dwashington102 <dwashington102@gmail.com>
2019-10-12 12:56:26 -0500

    Added lower() and capitalize() to input statements

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -26,8 +26,8 @@ def display_menu():
 
 
 def get_players():
-    player_1 = input('Player 1 Name: ')
-    player_2 = input('Player 2 Name: ')
+    player_1 = input('Player 1 Name: ').capitalize()
+    player_2 = input('Player 2 Name: ').capitalize()
     print()
     play_game(player_1, player_2)
 
@@ -37,44 +37,47 @@ def play_game(player_1, player_2):
     total_p2_wins = 0
     play_again = 'y'
     while play_again != 'n':
-        p1_move = input('Player-1 enter rock, paper, or scissors: ')
-        p2_move = input('Player-2 enter rock, paper, or scissors: ')
+        p1_move = input('Player-1 {}: Enter rock, paper, or scissors: '.format(player_1)).lower()
+        p2_move = input('Player-2 {}: Enter rock, paper, or scissors: '.format(player_2)).lower()
 
         if p1_move == p2_move:
             print("Player 1 Move", p1_move)
             print("Player 2 Move", p2_move)
             print('We have a tie')
+        elif p1_move == 'rock':
+            if p2_move == 'paper':
+                print('Player 2 wins!')
+                total_p2_wins +=1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins +=1
+
+        elif p1_move == 'scissors':
+            if p2_move == 'rock':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+
+        elif p1_move == 'paper':
+            if p2_move == 'scissors':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
         else:
-            if p1_move == 'rock':
-                if p2_move == 'paper':
-                    print('Player 2 wins!')
-                    total_p2_wins +=1
-                else:
-                    print('Player 1 wins!')
-                    total_p1_wins +=1
-            elif p1_move == 'scissors':
-                if p2_move == 'rock':
-                    print('Player 2 wins!')
-                    total_p2_wins += 1
-                else:
-                    print('Player 1 wins!')
-                    total_p1_wins += 1
+            print('Invalid entry received.  Try again')
 
-            elif p1_move == 'paper':
-                if p2_move == 'scissors':
-                    print('Player 2 wins!')
-                    total_p2_wins += 1
-                else:
-                    print('Player 1 wins!')
-                    total_p1_wins += 1
 
-            print('DEBUG >>> p1 wins', total_p1_wins)
-            print('DEBUG >>> p2 wins', total_p2_wins)
-            play_again = input('Do you want to play again (y/n): ')
+            #print('DEBUG >>> p1 wins', total_p1_wins)
+            #print('DEBUG >>> p2 wins', total_p2_wins)
+        play_again = input('Do you want to play again (y/n): ').lower()
 
     print()
-    print('Player-1 {} '.format(player_1), 'Total Wins: ', total_p1_wins, '.', sep='')
-    print('Player-2 {} '.format(player_2), 'Total Wins: ', total_p2_wins, '.', sep='')
+    print('Player-1 {} '.format(player_1), 'Total Wins: ', total_p1_wins, sep='')
+    print('Player-2 {} '.format(player_2), 'Total Wins: ', total_p2_wins, sep='')
 
 
 main()
