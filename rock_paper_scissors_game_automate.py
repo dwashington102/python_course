7b141b8f2242924a8cd3aec9d52d379a635aab9e
dwashington102 <dwashington102@gmail.com>
2019-10-16 00:48:39 -0500

    Last failure in description section
     File "rock_paper_scissors_game_automate.py", line 205, in <module>
        main()
      File "rock_paper_scissors_game_automate.py", line 22, in main
        start_game()
      File "rock_paper_scissors_game_automate.py", line 43, in start_game
        num_of_players = int(float(input('Enter # of player(s): ')))
    ValueError: could not convert string to float: 'Rick'

---
+++ rock_paper_scissors_game_automate.py
@@ -0,0 +1,207 @@
+# Make a two player Rock-Paper-Scissors game.  Ask for the players’ names. On each play, have
+# the players enter their move choice (rock, paper, or scissors). Then compare the moves,
+# and print out the players’ moves, a message of congratulations to the winner,
+# and ask if the players want to play again.
+#
+# Keep a running total of the wins/losses for each player, and when the players end the game,
+# print out the final wins / losses results for the players.
+# Remember the game rules:
+# - Rock beats scissors
+# - Scissors beats paper
+# - Paper beats rock
+
+# Import modules
+import random
+
+# Define GLOBAL CONSTANTS
+DISPLAY_SPACER = "="                            # Set character "=" used for header
+
+
+def main():
+    display_menu()
+    start_game()
+    final_results(player_1, player_2,total_p1_wins, total_p2_wins)
+
+
+# Print the banner header
+def display_menu():
+    print(DISPLAY_SPACER * 50)
+    print('Rock Paper Scissor Game')
+    print(DISPLAY_SPACER * 50)
+
+
+# start_game() gathers the user name(s) and calls get_*_players functions
+def start_game():
+    stop_game = 'start'
+    while stop_game != 'n':
+        print('How many players for the game?')
+        print('Choices\n')
+        print('Player-1 vs. Computer\t::\t<Select 1> ')
+        print('Player-1 vs. Player-2\t::\t<Select 2> ')
+        print('Automated 2-Player   \t::\t<Select 9> ')
+        print()
+        num_of_players = int(float(input('Enter # of player(s): ')))
+        if num_of_players == 1:
+            get_one_player()
+        elif num_of_players == 2:
+            get_two_player()
+        elif num_of_players == 9:
+            get_random_player()
+        else:
+           print('Invalid Entry...try again!')
+
+        #stop_game = input('Do you want to play again(y/n: ').lower()
+
+# get_one_player function takes user input for Player_1 while setting player_2 to Computer passing to play_game_1p()
+def get_one_player():
+    player_1 = input('Player 1 Name: ').capitalize()
+    player_2 = "Computer"
+    print()
+    play_game_1p(player_1, player_2)
+
+
+# get_two_player function takes user input for player_1  and player_2 and passes to play_game_2p ()
+def get_two_player():
+    player_1 = input('Player 1 Name: ').capitalize()
+    player_2 = input('Player 2 Name: ').capitalize()
+    print()
+    play_game_2p(player_1, player_2)
+
+
+# get_two_player function takes user input for player_1  and player_2 and passes to play_game_9 ()
+def get_random_player():
+    player_1 = input('Player 1 Name: ').capitalize()
+    player_2 = input('Player 2 Name: ').capitalize()
+    print()
+    play_game_9(player_1, player_2)
+
+
+
+# play_game() accepts player_1 variable, sets total wins for each user to 0,
+# creates options_list[], sets play_again sentinel value, and plays rock-paper-scissors
+def play_game_1p(player_1, player_2):
+    player_2 = 'Computer'
+    total_p1_wins = 0
+    total_p2_wins = 0
+    options_list = ['rock', 'paper', 'scissors']
+    play_again = 'y'
+
+    while play_again == 'y':
+        print()
+        p1_move = input('Player-1 {}: rock, paper, or scissors?: '.format(player_1)).lower()
+        random_play = random.randint(0, 2)
+        p2_move = options_list[random_play]
+        print('Player-2 move = ', p2_move)
+        print()
+
+        if p1_move == p2_move:
+            print("Player 1 Move", p1_move)
+            print("Player 2 Move", p2_move)
+            print('We have a tie')
+        elif p1_move == 'rock':
+            if p2_move == 'paper':
+                print('Player 2 wins!')
+                total_p2_wins +=1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins +=1
+
+        elif p1_move == 'scissors':
+            if p2_move == 'rock':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+
+        elif p1_move == 'paper':
+            if p2_move == 'scissors':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+        else:
+            print('Invalid entry received.  Try again')
+        print()
+        play_again = input('Do you want to play again? (y/n): ').lower()
+    final_results(player_1, player_2, total_p1_wins, total_p2_wins)
+
+
+def final_results(player_1, player_2, total_p1_wins, total_p2_wins):
+    print()
+    print('Player-1 {} '.format(player_1), 'Total Wins: ', total_p1_wins, sep='')
+    print('Player-2 {} '.format(player_2), 'Total Wins: ', total_p2_wins, sep='')
+
+
+# play_game_2p accepts player_1 and player_2 ariables sets total wins for each user to 0,
+# creates options_list[], sets play_again sentinel value, and plays rock-paper-scissors
+def play_game_2p(player_1, player_2):
+    #player_2 = 'Computer'
+    total_p1_wins = 0
+    total_p2_wins = 0
+    options_list = ['rock', 'paper', 'scissors']
+    play_again = 'y'
+
+    while play_again == 'y':
+        print()
+        p1_move = input('Player-1 {}: rock, paper, or scissors: '.format(player_1)).lower()
+        p2_move = input('Player-2 {}: rock, paper, or scissors: '.format(player_2)).lower()
+        print()
+
+        if p1_move == p2_move:
+            print("Player 1 Move", p1_move)
+            print("Player 2 Move", p2_move)
+            print('We have a tie')
+        elif p1_move == 'rock':
+            if p2_move == 'paper':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+
+        elif p1_move == 'scissors':
+            if p2_move == 'rock':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+
+        elif p1_move == 'paper':
+            if p2_move == 'scissors':
+                print('Player 2 wins!')
+                total_p2_wins += 1
+            else:
+                print('Player 1 wins!')
+                total_p1_wins += 1
+        else:
+            print('Invalid entry received.  Try again')
+
+        print()
+        play_again = input('Do you want to play again? (y/n): ').lower()
+    final_results(player_1, player_2, total_p1_wins, total_p2_wins)
+    #print()
+    #print('Player-1 {} '.format(player_1), 'Total Wins: ', total_p1_wins, sep='')
+    #print('Player-2 {} '.format(player_2), 'Total Wins: ', total_p2_wins, sep='')
+
+
+def play_game_9(player_1, player2):
+    total_p1_wins = 0
+    total_p2_wins = 0
+    options_list = ['rock', 'paper', 'scissors']
+    play_again = 'y'
+
+    while play_again == 'y':
+        print("Made it to game 9")
+        print()
+        print('Leaving 9')
+        print()
+        play_again = input('Do you want to play again? (y/n): ').lower()
+    final_results(player_1, player_2, total_p1_wins, total_p2_wins)
+
+
+main()
+
+
