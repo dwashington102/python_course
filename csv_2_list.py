8c69aed5d655403e0ea57179ce818283aa3a088d
David <dwashington102@gmail.com>
2019-10-27 21:05:49 -0500

    Bonus round on setting dividend summary

--- csv_2_list.py
+++ csv_2_list.py
@@ -6,8 +6,7 @@ The program will sum all values and print out a total value for dividend payouts
 import csv                  # Import csv module
 
 
-
-
+# Define Variables
 divends = []                # Create an empty list
 tot_div = 0.0                 # Set the total dividend payout to 0.00
 
@@ -20,6 +19,11 @@ tot_div = 0.0                 # Set the total dividend payout to 0.00
 # cannot be converted to a float.  In order to avoid this issue we must use the replace() to 
 # remove unwanted commas
 
+# Steps to create the csv file
+# Login to Fidelity --> Download transactions for selected time frame --> save to csv file
+# Grep and awk command to extract DIVIDEND entries from the csv file
+# grep DIV history.csv | awk -F",\"" '{print $2}'  | awk -F"\"" '{print $1}' >! /tmp/afile.tx
+
 with open('/tmp/afile.txt', 'r') as afile:
     row = csv.reader(afile, delimiter='\n')
     for r in row:
@@ -27,4 +31,12 @@ with open('/tmp/afile.txt', 'r') as afile:
         my_div = float(my_div.replace(',', ''))
         tot_div = tot_div + my_div
 
-print('Total Dividends YTD: ${:,.2f}'.format(tot_div))
+afile.close()
+print('Total Dividends 2017 - 2019 (YTD): ${:,.2f}'.format(tot_div))
+
+
+# Next step
+# 1 >>> Extract Fund Information
+# 2 >>> Total dividends to extracted fund 
+# 3 >>> After completing work for all funds - sum all divends compard to tot_div to confirm value
+#       is valid
