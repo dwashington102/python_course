69bef57e5becebd986ff57e1db6880d2e38cfeee
dwashington102 <dwashington102@gmail.com>
2019-11-04 22:25:40 -0600

    Build list, pull random item from list, and pass random item to anoter function

---
+++ change_wallpaper.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import random
+import time
+import os.path
+from os import path
+
+import gi
+gi.require_version('Gtk', '3.0')
+from  gi.repository import Gio, Gtk
+
+#Define GLOBAL CONSTANTS
+MYPATH='/usr/share/backgrounds/various'
+
+#Define variables
+
+
+def main():
+    check_mypath()
+    get_wallpaper()
+
+
+# Function will confirm if directory defined by MYPATH exists.  If not the program exists
+def check_mypath():                 
+    test_dir = (path.isdir(MYPATH))
+    if test_dir == True:   #Boolean check
+        time.sleep(3)      #Sleep just for the heck of it
+    else:
+        print('PATH:', MYPATH, " does not exists")
+        print('Exiting...')
+        exit(1)
+
+def get_wallpaper():    # Builds a list of files in MYPATH directory
+    wallpaper_lst = []
+
+    for (dirpath, dirname, filenames) in os.walk(MYPATH):
+        for afile in filenames:
+            wallpaper_lst.append(afile)
+
+    mytest = len(wallpaper_lst)
+    #time.sleep(5)
+    my_rand = random.randint(0, mytest) 
+    wallpaper = wallpaper_lst[my_rand]    #Sets the wallpaper to a random item from the wallpaper_lst
+    set_wallpaper(wallpaper)
+
+
+# Uses gi module(s) to pass values to Gnome gsettings
+def set_wallpaper(wallpaper):
+    settings = Gio.Settings.new("org.gnome.desktop.background")
+    settings.set_string("picture-uri", "file://" + MYPATH + '/' + wallpaper)
+    settings.apply()
+
+
+main()
+
+if __name__ == 'main':
+    main()
+
+
