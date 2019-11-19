1af0310f6f752d46c81e9e9c9b6505a630230a63
dwashington102 <dwashington102@gmail.com>
2019-11-18 21:30:50 -0600

    Begin work wtih CSV files and mysql db

--- csv_to_vmstatdb_mysql.py
+++ csv_to_vmstatdb_mysql.py
@@ -1,59 +1,50 @@
 # Import the required modules
-import mysql.connector
 from  pathlib import Path
 import os
+
+# 
+import time
+
+# Modules for csv and database operations
 import csv
-# Added the following module due to this error:
-#   File "csv_to_vmstatdb_mysql.py", line 40, in export_csvfile
-#   csv_data = csv.reader(file(csvfile))
-#   TypeError: '_io.TextIOWrapper' object is not callable
-from io import TextIOWrapper
+import pandas as pd
+from pandas.io import sql
+from sqlalchemy import create_engine
 
 
-# Set the GLOBAL CONSTANTS
 # Define GLOBAL CONSTANTS
 HOMEDIR = str(Path.home())
 CSVFILELOC = HOMEDIR + '/sqlite_db/csv_files/'
 
 def main():
     get_csvfiles()
-    export_csvfile()
 
 
+# Step through list of csv files in the given USERS $HOME/sqlite_db/csv_files path
 def get_csvfiles():
-    list_csvfiles = []
     for (read, directory, file) in os.walk(CSVFILELOC):
         try: 
             for csvfile in file:
                 csvfile = csvfile.rstrip('\n')
                 csvfile = CSVFILELOC + csvfile
-                list_csvfiles.append(csvfile)
-                export_csvfile(csvfile)
+                print('Filename being processed...\t', csvfile)
+                time.sleep(2)
+                export_csvfile(csvfile) # Pass a single csv file to be processed as a df for pandas.
         except FileExistsError:
             print('\nProblem with CSV files\n')
 
 
 def export_csvfile(csvfile):
-    db = mysql.connector.connect(
-        host="localhost",
-        user="devdavid",
-        password="h3lpm3",
-        database="CRASHDB"
-        )
+    df = pd.read_csv(csvfile, sep=':')
+    #df = pd.read_csv(csvfile, columns=['HOSTNAME', 'FREEMEM', 'BUFFER', 'CACHE', 'DATE'])
+    #print(df)
 
-    with open(csvfile, 'r') as file:
-        connCRASHDB = db.cursor()
-        csv_data = csv.reader(file(csvfile))
-        csv_data = csv_data.rstrip('\n')
-        for row in csv_data:
-            connCRASHDB.execute('INSERT INTO VSTATS (HOSTNAME, FREEMEM, BUFFER, CACHE, DATE)' 'VALUES("%s", "%s", "%s", "%s", "%s")', row)
-        connCRASHDB.commit()
-        connCRASHDB.close()
-        print 
-    file.close()
+    engine = create_engine('mysql://devdavid:h3lpm3@localhost/CRASHDB')
+    count = db.engine.
 
 if __name__ == "__main__":
     main()
+
 print()
 print('Exit program\n')
 exit(0)
