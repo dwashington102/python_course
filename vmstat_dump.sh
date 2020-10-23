5e15c92c67b1a38d9bcd360772f730c09ff1cabf
David Washington <dwashington102@gmail.com>
2020-10-22 20:26:34 -0500

    Replaced FQPath with /home/washingd added prereqs

--- vmstat_dump.sh
+++ vmstat_dump.sh
@@ -9,15 +9,15 @@ TSTAMP=$(date +"%Y%m%d%H%M%S")
 #echo "DEBUG: $HNAME$VMSTATS$TSTAMP"
 
 # Values
-CMD='/usr/bin/sqlite3 /home/devdavid/databases/sqlite_db/databases/vstats.db ".read /home/devdavid/databases/sqlite_db/SQL_files/import.sql"'
-COUNT=$(cat /home/devdavid/databases/sqlite_db/SQL_files/count.txt)
-LOGFILEDIR='/home/devdavid/databases/sqlite_db/log_files'
+CMD='/usr/bin/sqlite3 ${HOME}/databases/sqlite_db/databases/vstats.db ".read ${HOME}/databases/sqlite_db/SQL_files/import.sql"'
+COUNT=$(cat ${HOME}/databases/sqlite_db/SQL_files/count.txt)
+LOGFILEDIR='${HOME}/databases/sqlite_db/log_files'
 
 # update_csv function writes data to the vmstat_out.csv file
-update_csv(){ echo $HNAME$VMSTATS$TSTAMP >> /home/devdavid/databases/sqlite_db/csv_files/vmstat_out.csv ; }
+update_csv(){ echo $HNAME$VMSTATS$TSTAMP >> ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv ; }
 
 #reset_count function sets the counter value to 0 by updating the count.txt file
-reset_count(){ echo 0 > /home/devdavid/databases/sqlite_db/SQL_files/count.txt ; }
+reset_count(){ echo 0 > ${HOME}/databases/sqlite_db/SQL_files/count.txt ; }
 
 
 # If-Else loop actions -->
@@ -28,19 +28,19 @@ reset_count(){ echo 0 > /home/devdavid/databases/sqlite_db/SQL_files/count.txt ;
 # Current DB connection: engine = sqlalchemy.create_engine('mysql+pymysql://devdavid:h3...@localhost:3306/testdb2')
 
 if [ $COUNT -eq 0 ]; then
-	echo "HOSTNAME:FREEMEM:BUFFER:CACHE:DATE" > /home/devdavid/databases/sqlite_db/csv_files/vmstat_out.csv
-	echo $(( COUNT +1 )) > ~/databases/sqlite_db/SQL_files/count.txt
+	echo "HOSTNAME:FREEMEM:BUFFER:CACHE:DATE" > ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
+	echo $(( COUNT +1 )) > ${HOME}/databases/sqlite_db/SQL_files/count.txt
 elif [ $COUNT -lt 30 ]; then
 	update_csv
-	echo $(( COUNT +1 )) > ~/databases/sqlite_db/SQL_files/count.txt
+	echo $(( COUNT +1 )) > ${HOME}/databases/sqlite_db/SQL_files/count.txt
 else
 	update_csv
 
 	# Export data from csv file to the sqlite VSTATS database
-	#/usr/bin/sqlite3 /home/devdavid/databases/sqlite_db/databases/vstats.db ".read /home/devdavid/databases/sqlite_db/SQL_files/import.sql"
+	#/usr/bin/sqlite3 ${HOME}/databases/sqlite_db/databases/vstats.db ".read ${HOME}/databases/sqlite_db/SQL_files/import.sql"
 
 	# Export data from csv file to the mysql database
-        /usr/bin/python3 /home/devdavid/GIT_REPO/python_course/ex_csv_glob_pandas.py
+        /usr/bin/python3 ${HOME}/GIT_REPO/python_course/ex_csv_glob_pandas.py
 
 	# If the export is successful we write success to a log and the reset the counter to 0
 	if [ $? == 0 ];
@@ -52,7 +52,7 @@ else
 	# Else the export is unsuccessful and we write failure to a log and the reset the counter to 0
 		echo "DEBUG: Enter IF-FAIL"
 		echo "DEBUG: Failed imports $TSTAMP" >> $LOGFILEDIR/cron_fail.imports
-		mv  /home/devdavid/databases/sqlite_db/csv_files/vmstat_out.csv  /home/devdavid/databases/sqlite_db/csv_files/vmstat_out.csv_$TSTAMP	
+		mv  ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv  ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv_$TSTAMP	
 		reset_count
 	fi
 fi
