37682dc79aaf70faba7f399a3213606363fab541
dwashington102 <dwashington102@gmail.com>
2020-10-26 12:25:03 -0500

    Added comments to detail the various if-elif-else statements

--- vmstat_dump.sh
+++ vmstat_dump.sh
@@ -44,36 +44,43 @@ log_success(){ echo "Debug: Successful imports $TSTAMP" >> ${LOGSUCCESS} ; }
 log_failure(){ echo "Debug: Failed imports $TSTAMP" >> ${LOGFAIL} ; }
 
 if [ $COUNT -eq 0 ]; then
+	# This IF statement resets count to 0 then increments to 1 
 	#echo "HOSTNAME:FREEMEM:BUFFER:CACHE:DATE" > ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
 	echo "0" > ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_count.txt
 	echo $(( COUNT +1 )) > ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_count.txt
 elif [ $COUNT -lt 30 ]; then
+	# This elif statement updates the CSV file and increments by 1
 	update_csv
 	echo $(( COUNT +1 )) > ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_count.txt
 elif [ $COUNT -ge 30 ]; then
+	# This elif statement attempts to export contents of the csv file to the SQLite db
 	# Adding "grep import... " statement to test if the vmstat_db_import.sql file contains import command
 	grep import ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_import.sql
 	if [ $? != 0 ]; then
+		# IF statement will add import statement to vmstat_db_import.sql if missing, update csv file,  and then attempt export of csv file
 		echo ".import ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv VSTATS" >> ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_import.sql
 		update_csv
 		# Export data from csv file to the sqlite VSTATS database
 		/usr/bin/sqlite3 ${HOME}/databases/sqlite_db/databases/vstats.db ".read ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_import.sql"
 	else
+		# else statement will update csv file and attempt export of csv file
 		update_csv
-		# Export data from csv file to the sqlite VSTATS database
 		/usr/bin/sqlite3 ${HOME}/databases/sqlite_db/databases/vstats.db ".read ${HOME}/databases/sqlite_db/SQL_files/vmstat_db_import.sql"
 		if [ $? == 0 ]; then	
+			# if statement writes success msg, resets count, truncates csv file
 			if [ -f $LOGSUCCESS ]; then
 				log_success
 				reset_count
-				#truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
+				truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
 			else 
+				#else statement creates the success log, write success msg, resets count, truncates csf file
 				echo "CREATED SUCCESS LOG" >> $LOGSUCCESS
 				log_success
 				reset_count
-				#truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
+				truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
 			fi
 		else	
+			#else statement writes failure msg
 			if [ -f $LOGFAIL ]; then
 				log_failure
 			else
