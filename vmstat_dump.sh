a98fac012841530d2353bea340c5da0432bfbc77
dwashington102 <dwashington102@gmail.com>
2020-10-26 11:50:37 -0500

    Updated

--- vmstat_dump.sh
+++ vmstat_dump.sh
@@ -66,12 +66,12 @@ elif [ $COUNT -ge 30 ]; then
 			if [ -f $LOGSUCCESS ]; then
 				log_success
 				reset_count
-				truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
+				#truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
 			else 
 				echo "CREATED SUCCESS LOG" >> $LOGSUCCESS
 				log_success
 				reset_count
-				truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
+				#truncate -s 0 ${HOME}/databases/sqlite_db/csv_files/vmstat_out.csv
 			fi
 		else	
 			if [ -f $LOGFAIL ]; then
