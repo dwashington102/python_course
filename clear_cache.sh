4b72ff5bc438d028ad2ad3d83009c92d3e8e22ca
dwashington102 <dwashington102@gmail.com>
2021-01-16 08:40:48 -0600

    Added logging

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,28 +1,55 @@
 #!/bin/sh
 
-run_bleachbit (){
-	bleachbit -c firefox.cache google_chrome.cache opera.cache chromium.cache chromium.history chromium.cookies google_chrome.history vlc.mru
-}
+tStamp=`date +%Y%m%d_%H%M`
+logfile=$HOME/cronlogs/cron_run_$tStamp
+spacer='------------//------------------'
 
-truncate_vlc_history (){
-	truncate -s 0 $HOME/.config/vlc/vlc-qt-interface.conf
-}
 
 clear_files_recent (){
+	echo "\nStarting ${FUNCNAME}"
+	printf "clear_files_recent...\n" 
 	cp $HOME/bin/static/recently-used.xbel $HOME/.local/share/recently-used.xbel
 	cd $HOME/.cache/thumbnails
 	bleachbit -s `find . -type f -name "*.png"`
 }
 
+
+delete_cron_logs (){
+	printf "\nStarting ${FUNCNAME]}\n"
+	cd $HOME/cronlogs
+	bleachit -s `find . -type f -mtime +5`
+
+}
+
+
 delete_history (){
+	printf "\nStarting ${FUNCNAME}\n"
+	printf "delete_history...\n"
  	sed -i '/mp4/d' $HOME/.zsh_history
 }
 
+
+run_bleachbit (){
+	printf "\nStarting ${FUNCNAME}\n"
+	bleachbit -c firefox.cache google_chrome.cache opera.cache chromium.cache chromium.history chromium.cookies google_chrome.history vlc.mru
+}
+
+
+truncate_vlc_history (){
+	printf "\nStarting ${FUNCNAME}\n"
+	truncate -s 0 $HOME/.config/vlc/vlc-qt-interface.conf
+}
+
+
+
 MAIN() {
-run_bleachbit
-truncate_vlc_history
-clear_files_recent
-delete_history
+touch $logfile
+echo $spacer
+run_bleachbit >>  $logfile
+truncate_vlc_history >> $logfile
+clear_files_recent >> $logfile
+delete_history >> $logfile
+echo $spacer
 }
 
 MAIN
