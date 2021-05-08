f8a4ed1f211090c1922e06934aa58d9e37a53787
dwashington102 <dwashington102@gmail.com>
2021-05-08 14:07:02 -0500

    added check_conn_github() to test network connection

--- git_pull_startup.sh
+++ git_pull_startup.sh
@@ -12,7 +12,8 @@ dotfiles=$HOME/GIT_REPO/dotfiles
 timeStamp=`date +%Y%m%d_%H%M`
 currDir=$HOME/GIT_REPO
 
-function func_set_colors () {
+
+func_set_colors () {
     bold=$(tput bold)
     blink=$(tput blink)
     boldoff=$(tput sgr0)
@@ -24,11 +25,12 @@ function func_set_colors () {
     boldoff=$(tput sgr0)
 }
 
-function print_spacer (){
+func_print_spacer (){
+	printf "${normal}"
 	printf "\n\n\n"
 }
 
-function remove_30day_dirs (){
+func_remove_30day_dirs (){
 	cd  $GITDIR
 	for myDirs in `find . -maxdepth 1 -mtime +30  -name "*202*" -type  d`
 	    do
@@ -106,17 +108,34 @@ function check_dotfiles (){
     fi
 }
 
+func_check_conn_github () {
+    wget -q --spider www.github.com
+    if [ $? -ne 0 ]; then
+	printf "${red}"
+        printf "\nNetwork connection to github cannot be established."
+	printf "\nCheck network connection...exiting"
+	func_print_spacer
+	exit 100
+    else
+	printf "${green}"
+        printf "\nConnection to Github confirmed"
+	printf "${normal}"
+    fi
+}
 
 function MAIN (){
+func_set_colors
+func_check_conn_github
 cd $GITDIR
 check_pythoncourse
-print_spacer
+func_print_spacer
 func_pull_zsh_syntax
 print_spacer
 check_dotfiles
 print_spacer
-remove_30day_dirs
+func_remove_30day_dirs
 }
+
 
 
 # Where the magic happens
