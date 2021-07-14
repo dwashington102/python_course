889e6e4a01661fd1253e1390645531a68ab69932
dwashington102 <dwashington102@gmail.com>
2021-07-13 21:27:42 -0500

    Updated set_docker_cmd - More work to do

--- sys_admin_dir/set_docker_cmd.py
+++ sys_admin_dir/set_docker_cmd.py
@@ -9,17 +9,37 @@ Purpose: Test if the server is using docker or podman to manage containers
 import os
 import subprocess
 from sys import exit
-
-
-def main():
+from time import sleep
 
 
 def docker_test():
+    get_dockerCmd = "docker"
+    print('DEBUG >>> get_dockerCmd')
+    #get_dockerCmd = subprocess.call(get_dockerCmd, shell=True)
+    return_value = os.system('docker &> /dev/null')
+    sleep(10)
+    print("\nDocker Test Returns: ", get_dockerCmd)
+    print('returned value:  ', return_value)
+    
 
 def podman_test():
+    get_podmanCmd = "podman"
+    return_value = subprocess.call(get_podmanCmd, shell=True)
+    print("\nPodman Test Returns: ", return_value)
+    print('returned value:  ', return_value)
 
+def main():
+    print('DEBUG >>> in main')
+    docker_test()
+    podman_test()
 
+# Do work
+main()
 
-if __name__ == "main";
+if __name__ == "main":
     main()
+
+
+
+
 
