b036c14cb8dc2c2cf41aef56287ae2eb77f577b8
dwashington102 <dwashington102@gmail.com>
2021-07-17 23:07:49 -0500

    Updated version of docker5.sh

--- get_mp4_proj/docker5.sh
+++ get_mp4_proj/docker5.sh
@@ -1,21 +1,78 @@
 #!/usr/bin/env bash
-# Purpose: Create a docker container runs get_mp4_index.sh
-# The URL being passed on the 'docker container run...' line should include a valid URL followed by "/tags/"
-# 
-# Updated: Mon 05 Jul 2021 07:46:47 PM CDT
-# 
+# Script sets tags using the values in arr
+# -- downloads index.html from Site 
+# -- Locate docker image repo: getmp4
+# -- Builds docker container using using existing volume (-v) and passing command: ${getUrl}/tags/${myTag}
+# Where getUrl is site
+# tags = secondary entry at site
+# myTag is arr value
+
+# Site:
+# w_ap
 
 loopCount=1
 
-declare -a arr=("tag1" "tag2" "tag3")
+declare -a arr=("tag1" "tag2" "tag3" "tag4" "tag5")
 
-for myTag in "${arr[@]}"
-do
-    printf "\nCreating Docker Container"
-    docker container run -d --rm --name TESTDB_${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`" -v opendb:/data IMAGE-ID www.URL.com/tags/${myTag}
-    loopCount=$((loopCount + 1))
-    sleep 3
-done
-docker ps
+MAIN() {
+    func_getUserUrl
+    func_get_imageId
+    func_run
+}
 
+func_getUserUrl () {
+    printf "\nWhich URL: "
+    read getUrl
+    IFS=$'\n'
+    wget --no-check-certificate ${getUrl} -O index.html
+    if [ $? -eq 0 ]; then
+        ls -1 index.html > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            printf "\nDownload file name is not index.html"
+            printf "\n"
+	    exit 2
+        else
+            printf "\nDownload file name is index.html...beginning to process"
+            printf "\n"
+        fi
+    else
+        printf "\nwget failed to pull index file"
+        printf "\nConfirm the correct URL...exiting."
+        printf "\n"
+        exit 148
+    fi
+}
+
+
+func_get_imageId() {
+    # Gather a list of docker images repo name "getmp4"
+    docker images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
+    if [ "$?"  == "0" ]; then
+	# Added grep -m1 in order to restrict the number of images being returned
+        get_imgId=`docker images | grep -v ^REPO | grep -m1 getmp4 | awk '{print $3}'`
+        printf "\nContainer being built with image: ${get_imgId}"
+	printf "\n"
+	docker ps 
+    else
+	printf "\nUnable to locate Image Repository '${get_ImgId}'"
+	exit 1
+    fi
+}
+
+
+func_run() {
+    for myTag in "${arr[@]}"
+    do
+        printf "\nRetrieving: ${myTag}"
+        sleep 10
+        printf "\nCreating Docker Container"
+        # docker run statement is adding an arguement after the Image-ID
+        docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${myTag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${myTag}
+        loopCount=$((loopCount + 1))
+        sleep 3
+    done
+    docker ps
+}
+
+MAIN
 exit 0
