57b39f9abeef431235a346f80205907450113a01
dwashington102 <dwashington102@gmail.com>
2021-07-21 21:30:30 -0500

    Added docker6.sh

---
+++ get_mp4_proj/docker6.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+# Sets the container name (ex: TESTDB_v1, TESTDB_v2....)
+loopCount=1
+
+#declare -a my_arrlist=("tag1" "tag2" "tag3" "tag4" "tag5")
+
+MAIN() {
+    func_test_index
+    func_geturl
+    func_get_imageId
+    func_run
+}
+
+
+# test_index() renames ./index.html existing in current directory
+func_test_index() { 
+    if [ -f "./index.html" ]; then
+        printf "index.html exists"
+	    mv index.html index.html_`date +%Y%m%d_%H%M`
+    else
+	    printf "index.html does not exists..."
+    fi
+}
+
+# Prompts user for URL input using GUI
+func_diag_box() {
+    dialog --inputbox "Which URL:" 10 50 2>./url.txt
+    getUrl=`cat ./url.txt`
+    rm -rf ./url.txt
+    clear
+}
+
+# Prompts user for URL 
+func_geturl() {
+    type dialog > /dev/null 2>&1
+    if [ $? == 0 ]; then
+        func_diag_box
+    else
+    # Prompts user for URL using console text
+        printf "\nWhich URL: "
+        read getUrl
+    fi
+    #IFS= will cause problems for taglist array
+    #IFS=$'\n' <-----See line above!
+    wget --no-check-certificate ${getUrl} -O index.html
+    if [ $? -eq 0 ]; then
+        ls -1 index.html > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            printf "\nDownload file name is not index.html"
+            printf "\n"
+	    exit 2
+        else
+            printf "\nDownload file name is index.html...beginning to process"
+            printf "\n"
+        fi
+    else
+        printf "\nwget failed to pull index file"
+        printf "\nConfirm the correct URL...exiting."
+        printf "\n"
+        exit 148
+    fi
+}
+
+
+func_get_imageId() {
+    # Gather a list of docker images repo name "getmp4"
+    docker images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
+    if [ "$?"  == "0" ]; then
+	# Added grep -m1 in order to restrict the number of images being returned
+        get_imgId=`docker images | grep -v ^REPO | grep -m1 getmp4 | awk '{print $3}'`
+        printf "\nContainer being built with image: ${get_imgId}"
+	    printf "\n"
+    else
+	    printf "\nUnable to locate Image Repository '${get_ImgId}'"
+	    exit 1
+    fi
+}
+
+
+func_run() {
+    declare -a taglist
+    read -p "Insert Tags: " taglist
+    #read -p "Insert Tags separated by 'space': " tagList
+    for searchtag in ${taglist[@]}
+    do
+        printf "\nBuilding container for ${searchtag}"
+        printf "\n"
+        sleep 1
+        #printf "\nCreating Docker Container"
+        # docker run statement is adding an arguement after the Image-ID
+        docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${searchtag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${searchtag}
+        #docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${myTag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${myTag}
+        loopCount=$((loopCount + 1))
+        sleep 3
+        printf "\n"
+    done
+    docker ps
+    printf "\n"
+}
+
+MAIN
+exit 0
