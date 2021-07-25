4d1b88fcf5c7e61f0b4fe4be9ba6b69cbe017cf1
dwashington102 <dwashington102@gmail.com>
2021-07-25 09:47:47 -0500

    Added docker6.sh

--- get_mp4_proj/docker6.sh
+++ get_mp4_proj/docker6.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
-# Script sets tags using the values in arr
-# -- downloads index.html from Site 
-# -- Locate docker image repo: getmp4
-# -- Builds docker container using using existing volume (-v) and passing command: ${getUrl}/tags/${myTag}
-
-# Where getUrl is site
-# tags = secondary entry at site
-
-# Site:
-# w_ap
-
-# Building Container Details
-# Container is built using the following
-# - On HOST create /data
-#
-# - On Host create volume w_ap mount to /data
-# ---docker volume  create --driver local --opt type=none --opt device=/data/w_ap --opt o=bind w_ap
-#
-# - Create Ubuntu image naming "getmp4"
-# - Copying ./get_mp4_proj from HOST to Image 
-# When connecting to container start in this directory
-# WORKDIR /data
-# ENTRYPOINT [ "/bin/get_mp4_index.sh" ] 
-# --- docker build -t getmp4:v1 --label v1 .
-
+# Sets the container name (ex: TESTDB_v1, TESTDB_v2....)
 loopCount=1
+
+#declare -a my_arrlist=("tag1" "tag2" "tag3" "tag4" "tag5")
 
 MAIN() {
     func_test_index
     func_geturl
-    func_set_dockercmd
     func_get_imageId
     func_run
 }
@@ -84,26 +61,10 @@ func_geturl() {
     fi
 }
 
-func_set_dockercmd () {
-    which docker > /dev/null 2>&1
-    if [ "$?" == "0" ]; then
-        DOCKERCMD='docker'
-    else
-        which podman > /dev/null 2>&1
-        if [ "$?" == "0" ]; then
-            DOCKERCMD='podman'
-        else
-            printf "docker nor pomand commands found on server"
-            printf "Install the required packages...exiting"
-            exit 2
-        fi
-    fi
-}
 
 func_get_imageId() {
     # Gather a list of docker images repo name "getmp4"
-    # docker images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
-    ${DOCKERCMD} images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
+    docker images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
     if [ "$?"  == "0" ]; then
 	# Added grep -m1 in order to restrict the number of images being returned
         get_imgId=`docker images | grep -v ^REPO | grep -m1 getmp4 | awk '{print $3}'`
@@ -119,6 +80,7 @@ func_get_imageId() {
 func_run() {
     declare -a taglist
     read -p "Insert Tags: " taglist
+    read -p "Insert Category: "  get_category
     #read -p "Insert Tags separated by 'space': " tagList
     for searchtag in ${taglist[@]}
     do
@@ -127,9 +89,9 @@ func_run() {
         sleep 1
         #printf "\nCreating Docker Container"
         # docker run statement is adding an arguement after the Image-ID
+        docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${searchtag}" -v opendb:/data ${get_imgId} ${getUrl}/${get_category}/${searchtag}
+        # Line below is working as designed
         #docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${searchtag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${searchtag}
-        ${DOCKERCMD} container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${searchtag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${searchtag}
-        #docker container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${myTag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${myTag}
         loopCount=$((loopCount + 1))
         sleep 3
         printf "\n"
