7a9ceb26dcd30af94b5efe933cbe9080f0fa2394
dwashington102 <dwashington102@gmail.com>
2021-10-10 17:00:03 -0500

    Updated restart_network to remove cron logs older than 10 days

--- sys_admin_dir/restart_network.sh
+++ sys_admin_dir/restart_network.sh
@@ -2,32 +2,44 @@
 # 
 # Script checks the network connection sending a ping to www.yahoo.com
 # if the ping fails a restart NetworkManager.service takes place 
-# 2021-09-27
 
 timeStamp=$(date +%Y%m%d_%H%M)
 
 func_test_network (){
-    ping -i 1 -c3 www.yahoo.com
+    ping -i 1 -c3 8.8.8.8
     get_rc=$?
     if [ $get_rc != 0 ]; then  
         func_restart_network
     else
-        echo "Network Manager running at: ${timestamp} "  > /root/cron_network_running.log
+        echo "Network Manager running at: ${timeStamp} "  > /root/cron_network_running.log
     fi
 }
 
 
 func_restart_network (){
+    echo "Network Manager restart attemted at: ${timeStamp}" > /root/cron_network_down_${timeStamp}.log
+    systemctl is-active NetworkManager.service >> /root/cron_network_down_${timeStamp}.log
     systemctl restart NetworkManager.service
     if [ $? != 0 ]; then
-        echo "NetworkManager Restart Failed: " > /root/cron_network_startup_failed_${timeStamp}.log
+        echo "NetworkManager Restart Failed: " >> /root/cron_network_down_${timeStamp}.log
     else
-	echo "NetworkManager Restarted. " > /root/cron_network_restarted_${timeStamp}.log
+	echo "NetworkManager Restarted. " >> /root/cron_network_down_${timeStamp}.log
     fi
+}
+
+func_remove_logs_gt_10day (){
+    CRON_LOG_DIR='/root'
+    cd ${CRON_LOG_DIR}
+    get_logs=$(find . -mtime +10 -name 'cron_network*.log' | wc -l)
+    if [ ${get_logs} -gt 0 ]; then
+        find . -mtime +10 -name 'cron_network*.log' | xargs rm -f $1
+    fi
+
 }
 
 MAIN (){
     func_test_network
+    func_remove_logs_gt_10day
 }
 
 MAIN
