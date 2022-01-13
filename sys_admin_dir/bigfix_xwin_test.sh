5ee35bf2bb2a6d929875c80bf53a977b7a27b501
David Washington <washingd@us.ibm.com>
2022-01-13 15:08:00 -0600

    Simplified script by adding functions

--- sys_admin_dir/bigfix_xwin_test.sh
+++ sys_admin_dir/bigfix_xwin_test.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
+# 2022-11-13
+# Script will set a DISPLAY variable using the output of the 'w' command.
+# If the DISPLAY variable indicates a XWindow session is running - zenity is used to alert the user
+# If the DISPLAY variable indicates a terminal session is running - wall is used to alert the user
 
+
+# Function checks the output of the 'w' command extracting the first instance of indicating a XSession under Xorg
+# Example from the output of the 'w' command that indicates a running Xsession (Xorg)
+# myuser :1       :1   Mon19   ?xdm?   4:46m  0.00s /usr/libexec/gdm-x-session --register-session --run-script gnome-session
+get_xdisplay (){
+       get_xdisplay_var=$(w -h | /usr/bin/grep -m1 -E '[[:alnum:]]\s:[[:digit:]]\s' | awk '{print $2}')
+       if [ ! -z "$get_xdisplay_var" ]; then
+	       DISPLAY=${get_xdisplay_var}
+       fi
+}
+
+
+# Function checks the output of the 'w' command extracting the first instance of indicating a terminal session
+# Example from the output of the 'w' command that indicating a running terminal session
+# myuser pts/8    p     10:36    2:23m  0.26s  0.26s /usr/bin/zsh
+get_term (){
+       get_terms_var=$(w -h | /usr/bin/grep -m1 -E '[[:alnum:]]\s(pts|tty)' | awk '{print $2}')
+       if [ -z "$get_terms_var" ]; then
+	       printf "\nNo users currently connected"
+	       printf "\n"
+       else
+	       DISPLAY=${get_terms_var}
+       fi
+}
+
+MAIN (){
+DISPLAY=""
 file $(which w) &>/dev/null
 if [ $? == 0 ]; then
-       #XWindows Running Section	
-       # This command ONLY gathers the 1st instance of running XWindows session
-       get_display=$(w -h | grep -m1 -E '[[:alpha:]]\s:[[:digit:]]\s' | awk '{print $2}')
-       if [ -z "$get_display" ]; then
-	       #get_display is empty indicates connected users are not running Xwindows session
-       	       #The script should exit here because if XWindows is not running the registration.py will not run.
-	       printf "\n No connected users are running in XWindows session"
-	       printf "\n"
-	       exit 1
-	else
-	       DISPLAY=${get_display}
-	fi
+       get_xdisplay
+       if [ -z $DISPLAY ]; then
+             get_term
+       fi
 else
        #The w command was not found.
        # The w command is needed to gather a list of connected users, without the w command exit script here
        printf "\n 'w' command not found"
        printf "\n"
        exit 1
-
 fi
 printf "\nDISPLAY var: '$DISPLAY'"
 printf "\n"
-exit 0
+}
 
+MAIN
+exit 0
