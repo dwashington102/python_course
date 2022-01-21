68737033b566e7db84db4e0c398e34ea5b83cfea
dwashington102 <dwashington102@gmail.com>
2022-01-21 11:48:38 -0600

    Changed output format adding spacing.

--- baw_scripts/gc_elapsed_time.sh
+++ baw_scripts/gc_elapsed_time.sh
@@ -3,7 +3,8 @@
 read -p "Input GC Logname: " get_gc_log
 
 get_gc_longrunning=`egrep -m4  exclusive-"(start |end )" ${get_gc_log}`
-printf "\nDEBUG >>> ${get_gc_longrunning"
+printf "\n${get_gc_longrunning"
+printf "\n"
 #do
 #    printf "\nDEBUG >>> get_gc_longrunning: ${get_gc_longrunning}"
 #    printf "\n"
