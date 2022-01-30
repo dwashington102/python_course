cea8176dddeadb7f9ee2e7d47116049824c8d9fc
dwashington102 <dwashington102@gmail.com>
2022-01-30 09:21:27 -0600

    Testing creating funcs to update crontab

---
+++ sys_admin_dir/bf_update_cron.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+: <<'COMMENTS'
+This script is being used to test adding entries to the user's 
+crontab.  If the script works, we may add the functions in this 
+script into the bg_enforcementregistration.sh
+COMMENTS
+
+
+func_copy_crontab (){
+    if [ -f /tmp/bf_crontab.bak ]; then
+        command rm -f /tmp/bf_crontab.bak
+    fi
+    crontab -l > /tmp/bf_crontab.bak
+}
+
+func_update_crontab (){
+    echo "*/15 * * * * /tmp/bf_enforcementregistration.sh"  >> /tmp/bf_crontab.bak    
+    crontab /tmp/bf_crontab.bak 
+}
+
+func_remove_bf_entry_crontab (){
+    sed -i 's/.*bf_enforcementregistration.sh//' /tmp/bf_crontab.bak &>/dev/null
+    crontab /tmp/bf_crontab.bak 
+}
+
+MAIN (){
+    func_copy_crontab
+    func_update_crontab
+    func_remove_bf_entry_crontab
+}
+
+MAIN
+exit 0
