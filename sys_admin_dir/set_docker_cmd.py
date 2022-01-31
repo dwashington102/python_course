76afb6f8f263d50615e53c1d0f7c5dce626fb682
dwashington102 <dwashington102@gmail.com>
2022-01-30 22:38:28 -0600

    Replaced 'which' with 'command -v'

--- sys_admin_dir/set_docker_cmd.py
+++ sys_admin_dir/set_docker_cmd.py
@@ -14,14 +14,14 @@ from time import sleep, time
 def docker_test():
     #capture_output=True : blocks stderr from being returned to console
     print("Testing docker command...")
-    docker_cmd=subprocess.run(['which', 'docker'], capture_output=True)
+    docker_cmd=subprocess.run(['command', '-v' ,'docker'], capture_output=True)
     if docker_cmd.returncode==0:
         print('docker command found was successful')
         dockercmd='docker'
     else:
         print("Unable to find docker command")
         print("Testing podman command...")
-        podman_cmd=subprocess.run(['which', 'podman'], stdout=subprocess.DEVNULL)
+        podman_cmd=subprocess.run(['command', '-v', 'podman'], stdout=subprocess.DEVNULL)
         if podman_cmd.returncode==0:
             print('podman command found was successful')
             dockercmd='podman'
