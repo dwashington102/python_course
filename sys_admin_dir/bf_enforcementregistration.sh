13f59037c7058671b9170ad4bbc10db7420005a7
dwashington102 <dwashington102@gmail.com>
2022-02-10 19:43:44 -0600

    update get_xdisplay_var to grep $LOGNAME.*session

--- sys_admin_dir/bf_enforcementregistration.sh
+++ sys_admin_dir/bf_enforcementregistration.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-: <<'COMMENTS'
-This script is used by BigFix when laptops are not correctly registered.
-The script confirms if Xsession or Terminals are being used
-Confirm which version of Python is installed
-Copy the company provided reregister.py to /tmp
-The reregister.py is launched if Xsession is running
-The wall command is used if Terminal sessions only
-COMMENTS
 
 # Copies existing reregister.py to /tmp, changes the delay_run var to 1 resulting in a delay of only 1 second before GUI
 func_cp_rereg (){
 cp /opt/ibm/reregister/reregister.py /tmp/bf_reregister.py
 sed -i 's/delay_run = .*/delay_run = randint(0, 1)/' /tmp/bf_reregister.py
+
+# Remove entry from bf_reregister.py where the update_config script is called
+sed -i 's/subprocess.*update_config.*stdout//' /tmp/bf_reregister.py
+
+# Remove entries in bf_reregister.py where the beekeeper.ini file is deleted
+sed -i "s/os.remove.*beekeeper.ini\")/os.rename(\"\/tmp\/beekeeper.ini\", \"\/tmp\/bf_beekeeper.ini\")/" /tmp/bf_reregister.py
+
 chmod +x /tmp/bf_reregister.py
 }
 
@@ -35,12 +34,11 @@ func_check_running_reg () {
 	fi
 }
 
-#Confirm python3 is installed. If python3 is not installed attempt to throw wall message
+#Confirm python3 is installed. Exit if no version of python3 is installed.
 func_get_py_ver (){                                                                                                                                                                           
         command -v python3 &>/dev/null
         if [ $? != 0 ]; then
             printf "\nPython3 is not installed"
-            func_get_term
             exit 1
         else
             printf "\nPython3 is installed"
@@ -51,11 +49,12 @@ func_get_py_ver (){
 # Confirm if Xsession is running, set DISPLAY env, throw zenity popup before registration GUI is displayed.
 func_get_xdisplay (){
 	   bes_userid=$(env | command grep -m1 -E '(USER.*=|LOGNAME=)' | awk -F'=' '{print $2}')
-       get_xdisplay_var=$(w -h | grep -m1 $bes_userid | awk '{print $2}')
+	   get_xdisplay_var=$(w -h | command grep -m1 -E "^$bes_userid.*session" | awk '{print $2}')
 	   if [ ! -z "$get_xdisplay_var" ]; then
-              printf "\nCaptured Xsession"
-              DISPLAY=${get_xdisplay_var}
-			  func_check_zenity
+            printf "\nCaptured Xsession"
+            XDISPLAY=${get_xdisplay_var}
+	        printf "\nDEBUG >>>> DISPLAY= $XDISPLAY"
+            func_check_zenity
        fi
 }
 
@@ -65,30 +64,25 @@ func_get_term (){
 	   get_terms_var=$(w -h | grep -m1 $bes_userid | awk '{print $2}')
 	   #get_terms_var=$(w -h | /usr/bin/grep -m1 -E '[[:alnum:]]\s+(pts|tty)' | awk '{print $2}')
        if [ -z "$get_terms_var" ]; then
-               printf "\nNo users currently connected"
-               printf "\n"
+              printf "\nNo users currently connected"
+              printf "\n"
               exit 1
        else
-	          DISPLAY=${get_terms_var}
-              # 2022-02-04: Added following because older CentOS7 machines run a version of wall that does not accept a file as input
-              command wall -v &>/dev/null
-              if [ $? == "0" ]; then
-	   		      wall /tmp/bf_reg.msg
-              else
-                  wall < /tmp/bf_reg.msg
-              fi
+	          TERMDISPLAY=${get_terms_var}
+	          printf "\nDEBUG >>>> TERMDISPLAY= $XDISPLAY"
+	          wall /tmp/bf_reg.msg
        fi
 }
 
 MAIN (){
-DISPLAY=""
+XDISPLAY=""
 func_cp_rereg
 func_check_running_reg
 func_get_py_ver
 file $(which w) &>/dev/null
 if [ $? == 0 ]; then
        func_get_xdisplay
-       if [ -z $DISPLAY ]; then
+       if [ -z $XDISPLAY ]; then
              func_get_term
        fi
 else
@@ -98,9 +92,9 @@ else
        printf "\n"
        exit 1
 fi
-printf "\nDISPLAY var: '$DISPLAY'\n"
 zenity --warning --width=400 --height=200 --text '<b><span foreground="red">***** THIS COMPUTER IS NOT REGISTERED *****\n</span></b>\nRegistration is required when accessing internal IBM resources.\n\nMinimize all windows and click OK button to launch Registration GUI.\n\nComplete the Registration GUI' &>/dev/null
-$PYCMD /tmp/bf_reregister.py &
+$PYCMD /tmp/bf_reregister.py
 }
 
 MAIN
+exit 0
