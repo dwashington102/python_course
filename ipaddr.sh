cda796ac0b35ae62423e90fddbdbb4db04a09740
dwashington102 <dwashington102@gmail.com>
2022-02-22 14:09:00 -0600

    Adding usage statement to script

--- ipaddr.sh
+++ ipaddr.sh
@@ -2,6 +2,35 @@
 
 # Script simplies the output of the 'ip' command used to view the IP address, current state, and MTU of interfaces
 
+
+if [ "$1" = "--all" -o "$1" = "-a" ]; then
+    get_all
+    exit 0
+fi
+
+if [ "$1" = "--help" -o "$1" = "-h" ]; then
+    usage 
+    exit 0
+fi
+
+if [ -n "$1" ]; then
+    printf "\nUnknown parameter '$1'\n" >&2
+    usage
+    exit 1
+fi
+
+
+# Usage function
+usage(){
+    cat << EOF
+Usage: $0 [--help|--all]
+
+    -a, --all    Will print the network information for all interfaces
+    -h, --help   Show this help
+EOF
+}
+
+get_all (){
 # Write each interface to an array
 get_intface=($(ip a | command grep mtu | awk -F":" '{print $2}'))
 IFS=$'\n'
@@ -15,5 +44,6 @@ else
     printf "\nThe 'ip a' command failed to return any interfaces"
 fi
 printf "\n"
+}
 
 exit 0
