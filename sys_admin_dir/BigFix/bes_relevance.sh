fe1313821adc7f8ec3b13d530c29a719b478d656
dwashington102 <dwashington102@gmail.com>
2022-03-07 11:36:59 -0600

    Parse BES Client log

--- sys_admin_dir/BigFix/bes_relevance.sh
+++ sys_admin_dir/BigFix/bes_relevance.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/bash
-IFS=$'\n'
-loop_count=0
+loop_count=1
+array_count=0
 
+declare -a fixletArray
 printf "\n"
-for get_fixlet_id in $(command grep --max-count 10 --extended-regexp '.*Relevant.*\(fixlet:[[:digit:]].*\)$' "${PWD}"/TODAY.log | command grep --invert-match --ignore-case "not relevant")
+
+for get_fixlet_id in $(command grep --max-count 10 --extended-regexp '.*Relevant.*\(fixlet:[[:digit:]].*\)$' "${HOME}"/BESLogs/TODAY.log \ | command grep --invert-match --ignore-case "not relevant")
 do
-    loop_count=$((loop_count + 1))
+    IFS=$'\n'
     printf "%s: Full string NAME: %s\n" "${loop_count}" "${get_fixlet_id}"
     get_fixlet_num=$(echo $get_fixlet_id | awk -F"\\\(fixlet:" '{print $2}' | awk '{gsub(/)/, "") ; print }')
     printf "Fixlet Num: %s\n" "${get_fixlet_num}"
     printf "\n"
+    fixletArray[$array_count]="${get_fixlet_num}"
+    loop_count=$((loop_count + 1))
+    array_count=$((array_count + 1))
+    printf "\nDEBUG >>> %s" "${fixletArray[$array_count]}"
 done
+printf "\n"
+
+printf "\nDEBUG >>> fixletArray Total: %s" "${#fixletArray[@]}"
+printf "\nDEBUG >>> fixletArray Elements %s" "${fixletArray[*]}"
 printf "\n"
 
