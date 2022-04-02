bf27b620a51b41f77e87e6494253dd75708fac89
dwashington102 <dwashington102@gmail.com>
2022-04-02 07:57:26 -0500

    Updated format only.

--- sys_admin_dir/BigFix/bf_google_chrome_update.sh
+++ sys_admin_dir/BigFix/bf_google_chrome_update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -x
+#!/bin/bash 
 
 bf_temp_dir="/tmp/bf_upgrade-chrome"
 logfile="$bf_temp_dir/bf_upgrade_chrome.log"
@@ -7,7 +7,7 @@ check_dnf (){
     is_dnf_running=( $(ps -e | grep -E '(dnf|rpm)') )
 	if [ "${{#is_dnf_running[@]}" -ne 0 ]; then
 	    echo "Another DNF process is running...exiting"
-		exit 4
+	    exit 4
 	fi
 }
 
@@ -21,9 +21,9 @@ backup_rpmdb (){
 	    echo "Backing up RPM database (/var/lib/rpm)"
 	    tar czvf /root/bf_rpmdatabase.tar.gz /var/lib/rpm
 	    if [ "$?" != 0 ]; then
-		    echo "Unable to backup RPM database"
-		    echo "Upgrade of Google Chrome will not continue...exiting"
-		    exit 3
+		echo "Unable to backup RPM database"
+		echo "Upgrade of Google Chrome will not continue...exiting"
+		exit 3
 	    fi
 	fi
 }
@@ -39,12 +39,12 @@ check_network (){
 do_upgrade (){
     get_chrome_install=( $(rpm -qa --qf "%{NAME}\n" | grep google-chrome) )
     if [ ${#get_chrome_install[*]} -ne 0 ]; then
-	    echo "Downloading google-chrome packages"
-	    dnf upgrade --downloadonly ${{get_chrome_install[*]} --downloaddir "$PWD" --repo google-chrome -y
-	    pkg_names=( $(ls -1 *rpm) )
-	    echo "Performing upgrade of google-chrome packages"
-	    rpm -Fvh ${{pkg_names[*]}
-	    echo "Completed upgrade of google-chrome packages"
+	echo "Downloading google-chrome packages"
+	dnf upgrade --downloadonly ${{get_chrome_install[*]} --downloaddir "$PWD" --repo google-chrome -y
+	pkg_names=( $(ls -1 *rpm) )
+	echo "Performing upgrade of google-chrome packages"
+	rpm -Fvh ${{pkg_names[*]}
+	echo "Completed upgrade of google-chrome packages"
     else
         echo "No google-chrome packages installed"
     fi
@@ -55,13 +55,13 @@ MAIN (){
     mkdir "$bf_temp_dir"   # <-- Comment this line when running in BigFix Action Script
     touch "$logfile"
     (
-	echo "Starting upgrade of Google Chrome Packages"
+    echo "Starting upgrade of Google Chrome Packages"
     pushd "$bf_temp_dir" &>/dev/null
-	check_dnf
+    check_dnf
     check_network
     backup_rpmdb
     do_upgrade
-	echo "Script completed"
+    echo "Script completed"
     ) > $logfile
 }
 
