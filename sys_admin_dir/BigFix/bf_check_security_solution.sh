77b9310af1ee1e61298303c570c3d47ac985c259
dwashington102 <dwashington102@gmail.com>
2022-04-06 06:28:49 -0500

    retab scripts

--- sys_admin_dir/BigFix/bf_check_security_solution.sh
+++ sys_admin_dir/BigFix/bf_check_security_solution.sh
@@ -6,38 +6,37 @@ func_get_os (){
     command -v hostnamectl
     if [ $? == "0" ]; then
         os_fedora=$(command hostnamectl | command grep -E 'Operating System' | command grep Fedora)
-	if [ -z "${os_fedora}" ]; then
+    if [ -z "${os_fedora}" ]; then
             os_redhat=$(command hostnamectl | command grep -E 'Operating System' | command grep -E 'Red Hat|Alma|Cent')
             if [ -z ${os_redhat} ]; then
-	        printf "\nInvalid Operating System: $(command hostnamectl | command grep -E 'Operating System')\n"
-		exit 1
-	    fi
-	fi
+            printf "\nInvalid Operating System: $(command hostnamectl | command grep -E 'Operating System')\n"
+        exit 1
+        fi
+    fi
      else
         os_fedora=$(command cat /etc/os-release | command grep -E ^'NAME' | awk -F'[""]' '{print $2}')
-	if [ -z $"{os_fedora}" ]; then
+    if [ -z $"{os_fedora}" ]; then
             os_redhat=$(command cat /etc/os-release | command grep -E ^'NAME' | awk -F'[""]' '{print $2}')
             if [ -z ${os_redhat} ]; then
-	        printf "\nInvalid Operating System: $(command grep -E ^'NAME' | awk -F'[""]' '{print $2}')\n"
-		exit 1
-	    fi
-	fi
+            printf "\nInvalid Operating System: $(command grep -E ^'NAME' | awk -F'[""]' '{print $2}')\n"
+        exit 1
+        fi
+    fi
       fi
 }
 
 func_check_rpm_installed (){
     if [ ! -z $"{os_fedora" ]; then
-        command rpm --quiet -qi mdatp
-	if [ $? == "0" ]; then
-
+        command rpm --quiet -qi mdatp && echo "MDATP installed" || echo "MDATP NOT FOUND...exiting 8" ; exit 8
+    fi
 }
 
 #func_is_service_enabled (){
-	systemctl is-enabled SERVICE -q
+    systemctl is-enabled SERVICE -q
 #}
 
 #func_is_service_running (){
-	systemctl is-active SERVICE -q
+    systemctl is-active SERVICE -q
 #}
 
 #func_install_service (){
