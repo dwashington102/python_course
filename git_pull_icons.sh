9bf747db0a15cee092b6e8002ce782ad3e27579e
dwashington102 <dwashington102@gmail.com>
2022-04-18 16:56:15 -0500

    Added rename_dirs()

--- git_pull_icons.sh
+++ git_pull_icons.sh
@@ -6,14 +6,28 @@ Script performs a 'git clone' of various repos that contains icons used on the w
 allows user to automate the process of keeping icons updated with the latest version
 COMMENT
 
-
 tStamp=$(date +%Y%m%d_%H%M)
 scriptName=`basename "$0"`
-logfile=$HOME/cronlogs/cronlog-"$scriptName"_"$tStamp"
-declare -a gitRepoList=("https://github.com/alvatip/Nordzy-icon.git" "https://github.com/bikass/kora" "/-/-branch Arc-ICONS --single-branch  https://github.com/rtlewis88/rtl88-Themes.git" "https://github.com/unc926/OSX_ONE" "https://github.com/zayronxio/Mkos-Big-Sur" "https://github.com/yeyushengfan258/We10X-icon-theme" "https://github.com/yeyushengfan258/Win10Sur-icon-theme.git")
+logfile=$HOME/cronlogs/cronlog-"$scriptName"_"$tStamp".log
+declare -a gitRepoList=("https://github.com/vinceliuice/Fluent-icon-theme.git" "https://github.com/alvatip/Nordzy-icon.git" "https://github.com/bikass/kora" "/-/-branch Arc-ICONS --single-branch  https://github.com/rtlewis88/rtl88-Themes.git" "https://github.com/unc926/OSX_ONE" "https://github.com/zayronxio/Mkos-Big-Sur" "https://github.com/yeyushengfan258/We10X-icon-theme" "https://github.com/yeyushengfan258/Win10Sur-icon-theme.git")
+
+rename_dirs (){
+     IFS=$'\n'
+     list_dirs=($(find . -maxdepth 1 ! -iname ".*" -type d -exec basename {} \;))
+     if [ "${#list_dirs[@]}" -ne "0" ]; then
+         for dirName in "${list_dirs[@]}"
+         do
+             printf "\nDir to rename - %s\n" "$dirName"
+             mv "$dirName" "$dirName"_"$tStamp" 
+         done
+      else
+              printf "\nNO Directories found...exiting 1"
+              exit 1 
+
+       fi
+}
 
 git_clone_actions () {
-    pushd $HOME/git_icons &>/dev/null
     IFS=$'\n'
     for gitRepo in "${gitRepoList[@]}"
     do
@@ -32,10 +46,10 @@ MAIN (){
     fi
 
     (
+    pushd $HOME/git_icons &>/dev/null
+    rename_dirs
     git_clone_actions
-    ) >> "$logfile"
+    ) > "$logfile"
 }
 
-
 MAIN
-
