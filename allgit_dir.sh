ac9b63694d5b55d9d9e073a6b4d1b9363849c2f3
dwashington102 <dwashington102@gmail.com>
2022-04-24 07:57:27 -0500

    Cleaned up scripts to provide useful output

--- allgit_dir.sh
+++ allgit_dir.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/bash
 
+:<<'COMMENT'
+Script gathers a list of files contained in each subdirectory
+COMMENT
 
-gitdirLoc='$HOME/GIT_REPO'
 
 IFS=$'\n'
-topdirArray=( $(find . -maxdepth 1 -type d -exec basename {} \; | sort -u | awk -F'.' '{print $1}' | sort -u))
-
+topdirArray=( $(find . -maxdepth 1 -type d -exec basename {} \; | sort -u | awk -F'.' '{print $1}' | sort -u) )
 
 arrCount=1
-printf "\n"
-for myDir in ${topdirArray[*]}
+for myDir in ${topdirArray[@]}
 do
-    printf "DEBUG >>> \n%s" "${topdirArray[*]}"
-    sleep 10
-    dirArray=( $(find . -maxdepth 1 -name "$myDir.*" -type d -exec {} \;))
-    printf "Count: $arrCount - $dirArray\n"
+    #printf "DEBUG >>> \n%s" "${topdirArray[*]}"
+    sleep 2
+    dirArray=( $(find "$myDir" -mindepth 1 -maxdepth 1 -type f -exec basename {} \;) )
+    #printf "Count: $arrCount - $dirArray\n"
+    printf "\n(%s) Dir Name: %s\n\tNumber of Files: %s" "$arrCount" "$myDir" "${#dirArray[@]}"
     arrCount=$((arrCount + 1 ))
+    printf "\n"
 done
+printf "\n"
