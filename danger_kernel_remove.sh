806a54f7ce56311da765555fb0ca5541fc2b24e0
dwashington102 <dwashington102@gmail.com>
2022-04-24 10:55:27 -0500

    Changed output

--- danger_kernel_remove.sh
+++ danger_kernel_remove.sh
@@ -5,34 +5,37 @@ The purpose of this script is to remove outdated/unwanted kernels from /usr/lib/
 There is no way this script should be run in Production without full filesystem backups
 being available.
 
-This is what happens when you wake up at 4 in the morning and realize "hey /usr/lib/modules"
+This is what happens when you wake up at 4am and realize "hey /usr/lib/modules
 has a lot more than 3 subdirectories".
 
 Steps to take before running this script:
-1) Confirm system can be restored from backup
+1) Confirm system can be restored from backup OR YOU may end up rebuilding
 2) Confirm "installonly_limit" setting in the /etc/dnf/dnf.conf is being used
 
-Details:
+Details of the script:
 1 - Gather the max. number of kernels setting from dnf.conf
-2 - Gather the total  number of "kernel-core" rpms installed
+2 - Gather the total number of "kernel-core" rpms installed
 3 - Count number to subdirectories in /usr/lib/module
 4 - Check if the total number of subdirectories is greater than installonly limit set in /etc/dnf/dnf.conf
     - If yes --> Check if total number of subdirectores is greater than # of kernel-core rpms installed
     - If yes --> Loop through dir_total[@] and delete subdirectories ONLY if the subdirectory name DOES NOT
     match kernel names included in kernel_rpms[@]
+
+EXIT CODES:
+        1: Non-root user attempted to run script
 WARNING
 
+# Standard function to enable colors for console output
 func_set_colors () {
     bold=$(tput bold)
     blink=$(tput blink)
-    boldoff=$(tput sgr0)
+    offall=$(tput sgr0)
     reverse=$(tput rev)
     red=$(tput setaf 1)
     green=$(tput setaf 2)
     yellow=$(tput setaf 3)
     cyan=$(tput setaf 6)
     normal=$(tput setaf 9)
-    boldoff=$(tput sgr0)
 }
 
 
@@ -55,8 +58,16 @@ get_dir_count (){
     printf "\nTotal Number of directories - %s" "${#dir_total[@]}"
 }
 
+func_check_uid (){
+        if [[ $(id -u) != 0 ]]; then
+            printf "Script must be ran as root...exit (1)\n"
+            exit 1
+        fi
+}
+
 
 MAIN (){
+    func_check_uid
     func_set_colors
     IFS=$'\n'
     printf "\n"
@@ -68,8 +79,8 @@ MAIN (){
         if [[ "${#dir_total[@]}" -gt "${#kernel_rpms[@]}" ]]; then
             printf "\n"
             printf "\nThe number of directories (%s) exceeds number of installed kernels" "${#dir_total[@]}"
-            printf "\n\n${bold}>>> DOING DANGEROUS WORK HERE <<<${normal}"
-            pirntf "\n"
+            printf "\n\n${bold}${red}${reverse}${blink}>>> DOING DANGEROUS WORK HERE <<<${offall}"
+            printf "\n"
             deleteCount=0
             saveCount=0
 
@@ -82,6 +93,9 @@ MAIN (){
                 if [[ "$?" -ne 0 ]]; then
                     deleteCount=$((deleteCount +1 ))
                     deleteDirs+=($dirName)
+                    #Uncomment following 2 lines to delete the unwanted directories
+                    #printf "\nDelete directory: %s\n" "$dirName"
+                    #rm --verbose -I --recursive "$dirName"
                 else 
                     saveCount=$((saveCount +1))
                     savedDirs+=($dirName)
