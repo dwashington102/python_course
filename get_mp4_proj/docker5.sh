e470f4a1e1ff5e9d3131353db1658ce2c8f8c412
dwashington102 <dwashington102@gmail.com>
2022-04-24 18:48:55 -0500

    See CHANGELOG

--- get_mp4_proj/docker5.sh
+++ get_mp4_proj/docker5.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
-# Script sets tags using the values in arr
-# -- downloads index.html from Site 
-# -- Locate docker image repo: getmp4
-# -- Builds docker container using using existing volume (-v) and passing command: ${getUrl}/tags/${myTag}
-# Where getUrl is site
-# tags = secondary entry at site
-# myTag is arr value
+
+:<<'COMMENTS'
+Script sets tags using the values in arr
+-- downloads index.html from Site 
+-- Locate docker image repo: getmp4
+-- Builds docker container using using existing volume (-v) and passing command: ${getUrl}/tags/${myTag}
+
+Where getUrl is site
+tags = secondary entry at site
+myTag is arr value
+
+EXIT CODES:
+   1: Container image "getmp4" NOT FOUND
+   2: podman nor docker is installed 
+   3: Name of download file is not index.html 
+   148: wget failed to pull index file
+
 
 # Site:
 # w_ap
+COMMENTS
+
+# 2022-04-24: Updated docker run command changing "tags" to "term"
 
 loopCount=$((RANDOM % 10 + 1))
 
@@ -47,7 +60,7 @@ func_getUserUrl () {
         if [ $? -ne 0 ]; then
             printf "\nDownload file name is not index.html"
             printf "\n"
-	    exit 2
+	    exit 3
         else
             printf "\nDownload file name is index.html...beginning to process"
             printf "\n"
@@ -63,10 +76,10 @@ func_getUserUrl () {
 
 func_get_imageId() {
     # Gather a list of docker images repo name "getmp4"
-    $DOCKERCMD images getmp4 | grep -m1 -v ^REPO  > /dev/null 2>&1
+    $DOCKERCMD images -n | grep -m1 getmp4  &>/dev/null
     if [ "$?"  == "0" ]; then
 	# Added grep -m1 in order to restrict the number of images being returned
-        get_imgId=`$DOCKERCMD images | grep getmp4 | grep -v ^REPO | grep -m1 getmp4 | awk '{print $3}'`
+        get_imgId=$($DOCKERCMD images -n | grep -m1 getmp4  | awk '{print $3}')
         printf "\nContainer being built with image: ${get_imgId}"
 	printf "\n"
 	$DOCKERCMD ps 
@@ -84,7 +97,7 @@ func_run() {
         sleep 10
         printf "\nCreating Docker Container\n"
         # docker run statement is adding an arguement after the Image-ID
-        $DOCKERCMD container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${myTag}" -v opendb:/data ${get_imgId} ${getUrl}/tags/${myTag}
+        $DOCKERCMD container run -d --env TERM=dumb --rm --name ${getUserUrl}v${loopCount} -w "/data/today/`date +%Y%m%d_%H%M%S`_${myTag}" -v opendb:/data ${get_imgId} ${getUrl}/term/${myTag}
         loopCount=$((loopCount + 1))
         sleep 3
     done
