9087d2eb5026dc164aaed8ec0d955f91a79483b4
dwashington102 <dwashington102@gmail.com>
2022-04-24 07:57:44 -0500

    Making notes for how-to

---
+++ shell_scripts-how-to/adding_color_to_output/output-colors.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Create a function that sets the desired colors
+func_set_colors () {
+    bold=$(tput bold)
+    blink=$(tput blink)
+    boldoff=$(tput sgr0)
+    reverse=$(tput rev)
+    red=$(tput setaf 1)
+    green=$(tput setaf 2)
+    yellow=$(tput setaf 3)
+    cyan=$(tput setaf 6)
+    normal=$(tput setaf 9)
+    boldoff=$(tput sgr0)
+}
+
+
+
+MAIN (){
+    func_set_colors
+    printf "\n${green}Color GREEN${normal}"
+    printf "\n${red}Color RED${normal}"
+    printf "\n%sColor CYAN %s\n" "$cyan" "$normal"
+}
+
+
+MAIN
---
+++ shell_scripts-how-to/working_with_arrays/array-of-files.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+:<<'COMMENT'
+COMMENT
+
+# Declaring an Array
+declare -a HostList=("k430-raptor" "x1-raptor" "p340-dove-docker" "p340-dove-fed35" "p50-raptor")
+
+# Generating an array
+myList=( $(find . -mindepth 1 -maxdepth 1 -type f -exec basename {} \;))
+
+# Get a total count of the items in the array
+printf "\nTotal Items In Array: %s" "${#myList[@]}"
+
+# Iterate through array
+loopCount=1
+for eachItem in ${myList[@]}
+do
+    printf "\n(%s) Filename: %s" "$loopCount" "$eachItem"
+done
+
+# Grep string in array
+# NOTE: The "--" option tells printf that whatever follows is NOT a command line option
+IFS=$'\n'
+printf -- "\nGrepping Through Array:\t"
+printf -- '%s\n' "${myList[@]}" | grep array
+printf -- "\n"
+
+# Alternative method to grep an array
+# IFS=$'\n'
+# echo "${myList[*]" | grep myfilename
+
+
