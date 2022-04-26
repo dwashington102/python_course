1c2d973c468cb63a475705fe8e7399ab748f7773
dwashington102 <dwashington102@gmail.com>
2022-04-26 07:13:57 -0500

    Updated alloff

--- shell_scripts-how-to/adding_color_to_output/output-colors.sh
+++ shell_scripts-how-to/adding_color_to_output/output-colors.sh
@@ -4,14 +4,13 @@
 func_set_colors () {
     bold=$(tput bold)
     blink=$(tput blink)
-    boldoff=$(tput sgr0)
+    alloff=$(tput sgr0)
     reverse=$(tput rev)
     red=$(tput setaf 1)
     green=$(tput setaf 2)
     yellow=$(tput setaf 3)
     cyan=$(tput setaf 6)
     normal=$(tput setaf 9)
-    boldoff=$(tput sgr0)
 }
 
 
