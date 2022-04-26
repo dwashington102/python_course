b66fa6982b4f84a9d1f960009e43ce02da9c6eec
dwashington102 <dwashington102@gmail.com>
2022-04-26 07:36:25 -0500

    Expanded COMMENT section

--- shell_scripts-how-to/adding_color_to_output/output-colors.sh
+++ shell_scripts-how-to/adding_color_to_output/output-colors.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/bash
 
+:<<'TPUTDOC'
+URL: https://www.linuxcommand.org/lc3_adv_tput.php
+
+Capname	Description
+bold	Start bold text
+smul	Start underlined text
+rmul	End underlined text
+rev	Start reverse video
+blink	Start blinking text
+invis	Start invisible text
+smso	Start “standout” mode
+rmso	End “standout” mode
+sgr0	Turn off all attributes
+setaf <value>	Set foreground color
+setab <value>	Set background color
+
+Value	Color
+0	Black
+1	Red
+2	Green
+3	Yellow
+4	Blue
+5	Magenta
+6	Cyan
+7	White
+8	Not used
+9	Reset to default color
+
+TPUTDOC
+
 # Create a function that sets the desired colors
 func_set_colors () {
     bold=$(tput bold)
