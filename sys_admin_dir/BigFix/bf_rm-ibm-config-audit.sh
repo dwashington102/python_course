45347b67b54985c8b1bd62a1f9c0cd40b55d0270
dwashington102 <dwashington102@gmail.com>
2022-04-28 20:42:05 -0500

    Script name change only

--- sys_admin_dir/BigFix/bf_rm-ibm-config-audit.sh
+++ sys_admin_dir/BigFix/bf_rm-ibm-config-audit.sh
@@ -14,31 +14,31 @@ bf_temp_dir="/tmp/bf_linuxatibm/$scriptName"
 timeStamp=$(date +%Y%m%d_%H%M)
 logfile="$bf_temp_dir/$timeStamp.log"                                                                                                                                                   
 
-delete_pkg (){													
+delete_pkg (){                                                  
 rpm -q ibm-config-audit
 if [ "$?" == "0" ]; then
     printf "\n\nStarting process to remove package\n"
-	# Remove the --test option in order to delete package
-	rpm --erase $(rpm -q --queryformat '%{NAME}\n' ibm-config-audit)
-	rpm -q ibm-config-audit 
-	if [ "$?" == "0" ]; then
-	    printf "\n\nPackage was not removed...disabling 10-linuxatibm.rules\n"
+    # Remove the --test option in order to delete package
+    rpm --erase $(rpm -q --queryformat '%{NAME}\n' ibm-config-audit)
+    rpm -q ibm-config-audit 
+    if [ "$?" == "0" ]; then
+        printf "\n\nPackage was not removed...disabling 10-linuxatibm.rules\n"
         if [ -f /etc/audit/rules.d/10-linuxatibm.rules ]; then
-		    printf "\n\nRemoving /etc/audit/rules.d/10-linuxatibm.rules\n"
+            printf "\n\nRemoving /etc/audit/rules.d/10-linuxatibm.rules\n"
             rm -f /etc/audit/rules.d/10-linuxatibm.rules
-			printf "rm return code=$?"
-			printf "\n\nRestarting audit service\n"
+            printf "rm return code=$?"
+            printf "\n\nRestarting audit service\n"
             systemctl restart auditd.service
-			systemctl status auditd.service
-		fi
-		exit 9
-	else
-	    printf "\n\nPackage ibm-config-audit was sucessfully removed."
-		exit 0
-	fi
+            systemctl status auditd.service
+        fi
+        exit 9
+    else
+        printf "\n\nPackage ibm-config-audit was sucessfully removed."
+        exit 0
+    fi
 else
     printf "\n\nibm-config-audit package not found"
-	exit 0
+    exit 0
 fi
 }
 
