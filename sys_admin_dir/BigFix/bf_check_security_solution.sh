f4033ce9cd9c885b43c46e2a4efbed40daa6a06f
dwashington102 <dwashington102@gmail.com>
2022-04-28 21:28:46 -0500

    added elif statement

--- sys_admin_dir/BigFix/bf_check_security_solution.sh
+++ sys_admin_dir/BigFix/bf_check_security_solution.sh
@@ -1,28 +1,40 @@
 #!/usr/bin/bash
 # Script confirms if the security solution is running on user machines
 
+:<<COMMENT
+Confirm if MDAPT
+
+EXIT CODES:
+1: hostnamectl command did not return Fedora, Red Hat, Alma, or CentOS
+8: MDATP package not installed
+127: hostnamectl command not found
+COMMENT
 
 func_get_os (){
-    command -v hostnamectl
+    hostnamectl &>/dev/null
     if [ $? == "0" ]; then
+        printf "\nEntered if"
         os_fedora=$(command hostnamectl | command grep -E 'Operating System' | command grep Fedora)
-    if [ -z "${os_fedora}" ]; then
+        if [ -z "${os_fedora}" ]; then
             os_redhat=$(command hostnamectl | command grep -E 'Operating System' | command grep -E 'Red Hat|Alma|Cent')
             if [ -z ${os_redhat} ]; then
             printf "\nInvalid Operating System: $(command hostnamectl | command grep -E 'Operating System')\n"
-        exit 1
+            exit 1
+            fi
         fi
-    fi
-     else
+    elif [ "$?" -gt "0" ]; then
+        printf "\nCommand hostnamectl NOT FOUND\n"
+        exit 127
+    else
         os_fedora=$(command cat /etc/os-release | command grep -E ^'NAME' | awk -F'[""]' '{print $2}')
-    if [ -z $"{os_fedora}" ]; then
+        if [ -z $"{os_fedora}" ]; then
             os_redhat=$(command cat /etc/os-release | command grep -E ^'NAME' | awk -F'[""]' '{print $2}')
             if [ -z ${os_redhat} ]; then
             printf "\nInvalid Operating System: $(command grep -E ^'NAME' | awk -F'[""]' '{print $2}')\n"
-        exit 1
+            exit 1
+            fi
         fi
     fi
-      fi
 }
 
 func_check_rpm_installed (){
@@ -32,11 +44,11 @@ func_check_rpm_installed (){
 }
 
 #func_is_service_enabled (){
-    systemctl is-enabled SERVICE -q
+    #systemctl is-enabled SERVICE -q
 #}
 
 #func_is_service_running (){
-    systemctl is-active SERVICE -q
+    #systemctl is-active SERVICE -q
 #}
 
 #func_install_service (){
@@ -45,6 +57,7 @@ func_check_rpm_installed (){
 
 MAIN (){
     func_get_os
+    func_check_rpm_installed
 }
 
 
