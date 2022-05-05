1cbadcbc4a1597a98b315469e11cc88718c4fb40
dwashington102 <dwashington102@gmail.com>
2022-05-05 18:32:15 -0500

    Script to stop BigFix components

---
+++ sys_admin_dir/BigFix/bes_stopEnv.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+bes_list=("besclient" "beswebreports" "beswebui"  "besgatherdb" "besfilldb" "besserver")
+
+func_besrunning() {
+	pgrep BES
+	[ "$?" == "0" ] && printf "\nBES Components are running....\n" || printf "\nNo BES Components running...exiting\n" ; exit 0
+}
+
+func_stopbes() {
+for besComp in ${bes_list[@]}
+do
+    printf "\nBES Component: %s" "$besComp"
+    printf "\nStatus: "
+    service "$besComp" status
+    if [ "$?" == "0" ]; then
+	    #printf "\nStopping $besComp..."
+	    service "$besComp" stop
+	    printf "\nReturn Code from stop command: $?"
+	    printf "\n"
+    else
+	    printf "\nService $besComp is down"
+	    printf "\n"
+    fi
+done
+}
+
+func_getpids() {
+    printf "\nRunning BES Components:\n"
+    ps -fp $(pgrep BES) | awk '{printf "\n%8s %6s %6s" $1,$2,$5,$9}' ; printf "\n"
+}
+
+
+
+MAIN (){
+    func_besrunning
+    func_getpids
+    func_stopbes
+}
+
+MAIN
