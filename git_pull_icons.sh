ac890886ba82dc5d1488cca5d653bb6974627bbf
David Washington <dwashington102@gmail.com>
2022-05-13 23:49:08 -0500

    updated COMMENT and  ${gitRepoList}

--- git_pull_icons.sh
+++ git_pull_icons.sh
@@ -4,12 +4,23 @@
 << 'COMMENT'
 Script performs a 'git clone' of various repos that contains icons used on the workstation
 allows user to automate the process of keeping icons updated with the latest version
+
+
+EXIT CODES:
+1 - No existing git repo directories found in $HOME/git_icons
 COMMENT
 
 tStamp=$(date +%Y%m%d_%H%M)
 scriptName=`basename "$0"`
 logfile=$HOME/cronlogs/cronlog-"$scriptName"_"$tStamp".log
-declare -a gitRepoList=("https://github.com/vinceliuice/Fluent-icon-theme.git" "https://github.com/alvatip/Nordzy-icon.git" "https://github.com/bikass/kora" "/-/-branch Arc-ICONS --single-branch  https://github.com/rtlewis88/rtl88-Themes.git" "https://github.com/unc926/OSX_ONE" "https://github.com/zayronxio/Mkos-Big-Sur" "https://github.com/yeyushengfan258/We10X-icon-theme" "https://github.com/yeyushengfan258/Win10Sur-icon-theme.git")
+declare -a gitRepoList=("https://github.com/vinceliuice/Fluent-icon-theme.git"
+ "https://github.com/alvatip/Nordzy-icon.git"
+ "https://github.com/bikass/kora"
+ "/-/-branch Arc-ICONS --single-branch  https://github.com/rtlewis88/rtl88-Themes.git" 
+ "https://github.com/unc926/OSX_ONE" 
+ "https://github.com/zayronxio/Mkos-Big-Sur" 
+ "https://github.com/yeyushengfan258/We10X-icon-theme" 
+ "https://github.com/yeyushengfan258/Win10Sur-icon-theme.git")
 
 rename_dirs (){
      IFS=$'\n'
