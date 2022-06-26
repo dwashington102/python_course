5dba6f7e73ce8a61970409341ff8cade64e48827
dwashington102 <dwashington102@gmail.com>
2022-06-25 23:06:09 -0500

    Added mkdir-p and touch biosfile

--- sys_admin_dir/BigFix/bf_tpm_status_v2.sh
+++ sys_admin_dir/BigFix/bf_tpm_status_v2.sh
@@ -3,9 +3,11 @@
 bf_temp_dir="/tmp/bf_linuxatibm"
 logfile="$bf_temp_dir/bf_tpm_status.log"
 timeStamp=$(date +%Y%m%d_%H%M)
-biosfile="/var/opt/beekeeper/bios.ini"
+biosfile="/tmp/bios.ini"
+#biosfile="/var/opt/beekeeper/bios.ini"
 
 get_tpm_info (){
+touch $biosfile
 if [ -r /sys/class/tpm/tpm0 ]; then
     get_tpm_ver=$(grep ^"[[:digit:]]" /sys/class/tpm/tpm0/tpm_version_major)
     if (( 1 <= "$get_tpm_ver" && "$get_tpm_ver" <= 99 )); then
@@ -19,6 +21,9 @@ fi
 }
 
 MAIN (){
+    if [ ! -d $bf_temp_dir ]; then
+        mkdir -p $bf_temp_dir
+    fi
     touch "$logfile"
     (
 	echo "Check TPM status - $timeStamp"
