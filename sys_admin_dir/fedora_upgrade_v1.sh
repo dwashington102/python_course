da445ef507ab3c342552e647da3736fa6cccfa41
dwashington102 <dwashington102@gmail.com>
2022-07-24 09:13:57 -0500

    Initail commit after testing

--- sys_admin_dir/fedora_upgrade_v1.sh
+++ sys_admin_dir/fedora_upgrade_v1.sh
@@ -2,36 +2,54 @@
 
 <<'COMMENTS'
 Script will run the commands necessary to upgrade Fedora to latest version.
-The script should be ran as the root user
+example: Fedora 35 --> to Fedora 36
+
+Enable the last 3 functions in MAIN() to force the upgrade to take place.
+
+Exit Codes:
+1 - Attempt to run script as non-root user
+2 - Fedora is not installed
 
 COMMENTS
 
 
+is_fedora (){
+    hostnamectl | grep Fedora
+    if [ $? != "0" ]; then
+        printf "\nFedora Not Installed"
+        exit 2
+    fi
+    printf "\nFedora is installed"
+}
+
+
 check_uid (){
-   printf "\nDEBUG:  $(id -u)\n"
-   if [ "$(id -u)" == "0" ]; then
-	   printf "\nRunning script as root...continue"  
-   else
+   if [ "$(id -u)" != "0" ]; then
 	   printf "\nScript must be ran by root user...exiting"
 	   exit 1
    fi
+   printf "\nRunning script as root...continue"  
    sleep 10
 }
+
 
 preupdate_fedora (){
    dnf -y --verbose --refresh update
    dnf -y --verbose upgrade
 }
 
+
 reboot_fedora (){
     printf "\nWork to do here"
 }
+
 
 system_upgrade (){
     echo "Fedora Version: "
     read fedVer
     dnf -y system-upgrade download --releasever="$fedVer"
 }
+
 
 upgrade_reboot (){
     dnf -y system-upgrade reboot
@@ -41,9 +59,10 @@ upgrade_reboot (){
 MAIN () {
     printf "\nRunning this script WILL REBOOT this machine"
     check_uid
-    preupdate_fedora
-    system_upgrade
-    upgrade_reboot
+    is_fedora
+    #preupdate_fedora
+    #system_upgrade
+    #upgrade_reboot
 }
 
 MAIN
