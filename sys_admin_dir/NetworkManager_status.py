268fb6433eac0921459f781c3817abf2fd6083a9
dwashington102 <dwashington102@gmail.com>
2022-07-25 20:26:19 -0500

    Added try-except re: createlog()

--- sys_admin_dir/NetworkManager_status.py
+++ sys_admin_dir/NetworkManager_status.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+'''
+Exit Codes
+1 - Server is running initd
+2 - Unable to start ssh
+
+'''
+
 import os
 import subprocess
 import sys
@@ -10,30 +17,38 @@ from clear_screen import clear
 from logfile_tstamp import get_timestamp
 from create_logfile import createlog
 
+
 def func_confirm_daemon():
-    get_sysd=str(subprocess.check_output(['ps', '-q', '1', '-o', 'comm=']).decode(sys.stdout.encoding))
+    get_sysd = str(subprocess.check_output(['ps', '-q', '1', '-o', 'comm='])
+                   .decode(sys.stdout.encoding))
     if get_sysd.startswith('system'):
         print('Server is running systemd')
     else:
         print('Server is running initd')
-        exit
+        sys.exit(1)
+
 
 def func_get_status_sysd():
-    get_stat=os.system('systemctl status sshd | grep "Active:.*active.*running.*since" > /dev/null')
+    get_stat = os.system('systemctl status sshd |'
+                        + 'grep "Active:.*active.*running.*since" > /dev/null')
     if get_stat != 0:
         print('\nNetworkManager is down, restarting...')
-        start_stat=os.system('systemctl start sshd > /dev/null')
+        start_stat = os.system('systemctl start ssh &> /dev/null')
         if start_stat != 0:
             print('Unable to start service....')
-            exit()
-    else: 
+            sys.exit(2)
+    else:
         print('\nNetworkManager is currently running')
-    
+
 
 def main():
     clear()
     func_confirm_daemon()
-    createlog()
+    try:
+        createlog()
+    except OSError as e:
+        print('\nERROR....{}'.format(e))
+        sys.exit(3)
     func_get_status_sysd()
 
 
@@ -41,5 +56,3 @@ main()
 
 if __name__ == "main":
     main()
-
-
