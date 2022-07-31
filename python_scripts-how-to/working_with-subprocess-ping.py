3fac9525fc5c480e73ad989f484088a1b9026ff3
dwashington102 <dwashington102@gmail.com>
2022-07-31 01:14:13 -0500

    Initial commit

---
+++ python_scripts-how-to/working_with-subprocess-ping.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+try:
+    ret = subprocess.call("ping -c1 8.8.8.8", shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+    s_ret = str(ret)
+    if s_ret == "0":
+        print('Server is available')
+    else:
+       print('Sever is unavailable')
+except:
+    raise
