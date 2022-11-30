4a4ad41bdf7a4d433d8ce2e0fa6e99844edec016
dwashington102 <dwashington102@gmail.com>
2022-11-30 10:14:24 -0600

    Updated to accept file ext. from cli when calling script

--- clean_files.sh
+++ clean_files.sh
@@ -6,13 +6,13 @@ COMMENTS
 
 
 func_dd () {
-    file_count=$(find . -maxdepth 1 -type f -name "*.${ext_type}" | wc -l)
+    file_count=$(find . -maxdepth 1 -type f -name "*${ext_type}" | wc -l)
     if [ ${file_count} -gt 0 ]; then
-        for getfile in $(ls -1 *.${ext_type})
+        for getfile in $(ls -1 *${ext_type})
             do
                 getsize=$(ls -l ${getfile} | awk '{print $5}')
                 echo "Filename: ${getfile} --- Size: ${getsize}"
-                sleep 5
+                sleep 1
                 # dd if=/dev/urandom of=${getfile} bs=${getsize} count=2 conv=notrunc
             done
     else
@@ -44,22 +44,30 @@ func_get_ext () {
 }
 
 
-MAIN () {
+main () {
     func_set_colors
-    printf "\nSCRIPT WILL dd ALL FILES IN THIS DIRECTORY"
+    printf "\nSCRIPT WILL dd ALL FILES IN THIS DIRECTORY:\t"
     echo $PWD
-    sleep 10
 
-    if [ ! -z "$1" ]; then
-        ext_type="$1"
+    if [ -z "${ext_type}" ]; then
+        func_get_ext
     fi
-    func_get_ext
+
+    if [ "${ext_type}" == "all" ]; then
+        ext_type="*"
+    fi
+
+    printf "Preparing to dd files with ext. ${ext_type}\n"
+    sleep 5
     func_dd
     file $(command bleachbit -v) &>/dev/null
     if [ "$?" == "0" ]; then
         printf "\nCalling bleachbit..."
-        bleachbit -s *.${ext_type} 2>/dev/null
+        bleachbit -s *${ext_type} 2>/dev/null
     fi
 }
 
-MAIN
+
+ext=("$@")
+ext_type=${ext[0]}
+main 
