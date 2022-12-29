15c893c13ac5b9ec01e114c493079765b650ab4e
dwashington102 <dwashington102@gmail.com>
2022-12-28 23:51:06 -0600

    Updated while loop adding a break

--- whiletest.py
+++ whiletest.py
@@ -12,24 +12,27 @@ y=int(x+10)
 loop=1
 
 while (x<=y):
-	if x==y-5:
-		str_x=str(x)
-		str_y=str(y)
-		str_loop=str(loop)
-		#print("Line Count: ", loop ,' X Value: ',x,' Y Value',y,'Only 4 more lines')
-		print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "] ONLY 4 MORE LINES")
-		x+=1
-		loop+=1
-	elif x!=y:
-		str_x=str(x)
-		str_y=str(y)
-		str_loop=str(loop)
-		#print('Line Count: '+ str_loop + ' X Value: ',x,' Y Value',y)
-		print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "]")
-		x+=1
-		loop+=1
-	else:
-		print('That\'s all folks')
-		exit(0)
+    if x==y-5:
+        str_x=str(x)
+        str_y=str(y)
+        str_loop=str(loop)
+        #print("Line Count: ", loop ,' X Value: ',x,' Y Value',y,'Only 4 more lines')
+        print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "] ONLY 4 MORE LINES")
+        x+=1
+        loop+=1
+    elif x!=y:
+        str_x=str(x)
+        str_y=str(y)
+        str_loop=str(loop)
+        #print('Line Count: '+ str_loop + ' X Value: ',x,' Y Value',y)
+        print("Line Count: "+ str_loop +" [X Value: " + str_x + " Y Value: " + str_y + "]")
+        x+=1
+        loop+=1
+    else:
+        print('That\'s all folks')
+        break
 
+
+print("Out of while loop")
+exit(0)
 
