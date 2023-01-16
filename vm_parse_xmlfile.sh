a86c418b33c4238008a1071f6d7dbf7642396a3d
dwashington102 <dwashington102@gmail.com>
2023-01-16 14:00:18 -0600

    Initial push. Script parses tags in VM XML file

---
+++ vm_parse_xmlfile.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/sh
+
+:<<'COMMENTS'
+Script prompts user to select a Virtual Manager Session ("user" or "system"),
+gathers a list of XML files associated with the session, and prompts user to
+select which XML file to parse.
+
+Using the select XML file, the script pulls a list of XML tags from the XML file,
+prompts the user to select a tag, and then pulls tag information from the file,
+and displays tag info to the console
+
+Exit Codes:
+1 - func_get_session(): Attempt to run script as root
+2 - func_get_session(): Invalid session name received
+3 - func_select_xml():  Invalid selection of XML files
+4 - func_ls_vms():  vmdir does not exists
+5 - func_ls_vms():  vmdir does not contain XML files
+7 - func_get_tags(): No tags found in XML file
+8 - func_tag_select(): Invalid selection of tag name
+COMMENTS
+
+
+func_get_session (){
+    shopt -s nocasematch
+    printf '\nIs Virtual Machine a "User" or "System" Session\n'
+    printf 'Type "user" or "system"\t'
+    read get_session
+
+    if [[ "$get_session" == "system" ]]; then
+        if [[ $(id -u) != 0 ]]; then                                                                                                                                                                                                                                           
+            printf "To view System Virtual Machines\n"
+            printf "Script must be ran as root...exit (1)\n"
+            exit 1
+        fi
+        vmdir="/etc/libvirt/qemu"
+    elif [[ "$get_session"  == "user" ]]; then
+        vmdir="$HOME/.config/libvirt/qemu"
+    else
+        printf "Invalid session entered...exit(2)\n"
+        exit 2
+    fi
+}
+
+
+func_ls_vms (){
+    if [[ ! -d ${vmdir} ]]; then
+        printf "${get_session} - ${vmdir} does not exists...exit(4)\n"
+        exit 4
+    fi
+
+    pushd ${vmdir} &>/dev/null
+
+    printf "\nList of Virtual Machines for userid $(id -un)"
+    printf "\n=========================================="
+    listFiles=( $(command ls -1 ${PWD} | command grep --regexp='.*.xml$')) 
+    len=${#listFiles[@]}
+    if [[ "${len}" == "0" ]]; then
+        printf "\nNo XML files found in ${vmdir}...exit(5)\n"    
+        exit 5
+    fi
+
+    xmlloopCount=0
+    for eachxml in ${listFiles[@]}
+    do
+        xmlloopCount=$((xmlloopCount+1))
+        printf "\n${xmlloopCount}: ${eachxml}"
+    done
+}
+
+
+func_select_xml (){
+    printf "\n"
+    printf "\nSelect number associated with XML file:\t"
+    read xmlnum
+    if [[ "$xmlnum" -lt 1 || "$xmlnum" -gt "$xmlloopCount" ]]; then
+        printf "\nInvalid selection...exit(3)"
+        exit 3
+    fi
+    arraynum=$((xmlnum-1))
+    xmlname=${listFiles[arraynum]}
+}
+
+
+func_get_tags (){
+# Loops through the XML file pulling the tags
+# Example of "cpu" tag in XML file
+#      <cpu mode='host-model' check='partial'/>                                                                                                                                                                                                                                    
+#        <clock offset='utc'>                                                                                                                                                                                                                                                        
+#         <timer name='rtc' tickpolicy='catchup'/>                                                                                                                                                                                                                                  
+#         <timer name='pit' tickpolicy='delay'/>                                                                                                                                                                                                                                    
+#         <timer name='hpet' present='no'/>
+
+    IFS=$'\n'
+    taglist=( $(command grep -E ^"\s{2}<" ${domainxml} | command grep -v -E "\s{2}</") )  
+    lentaglist=${#taglist[@]}
+    if [[ "$lentaglist" == "0" ]]; then
+        printf "\nNo tags found in ${domainxml}...exit(7)\n"
+        exit 7
+    fi
+
+    printf "Available XML Tags in ${domainxml}:"
+    tagloopCount=0
+    for tagraw in ${taglist[@]}
+    do
+    tagloopCount=$((tagloopCount+1))
+    echo "${tagraw}" | command grep -E "</" &>/dev/null
+        if [[ "$?" == "0" ]]; then
+            taglabel=$(echo "${tagraw}" | awk -F'[<>]' '{print $2}' | awk '{print $1}')
+        else
+            taglabel=$(echo "${tagraw}" | awk -F'[<>]' '{print $2}' | awk '{print $1}')
+        fi
+    printf "\n${tagloopCount}: ${taglabel}"
+    done
+    printf "\n"
+}
+
+
+func_select_tag (){
+    printf "\n"
+    printf "\nSelect number associated with TAGNAME:\t"
+    read tagnum
+    if [[ "$tagnum" -lt 1 || "$tagnum" -gt "$tagloopCount" ]]; then
+        printf "\nInvalid selection...exit(8)"
+        exit 8
+    fi
+    arraytag=$((tagnum-1))
+    tagname=${taglist[arraytag]}
+}
+
+
+main (){
+    if [[ -f "${domainxml}" ]]; then
+        export get_session="Custom"
+        func_get_tags
+        func_select_tag
+    else
+        if [[ $(id -u) != "0" ]]; then
+            func_get_session
+        else
+            vmdir="/etc/libvirt/qemu"
+            get_session="System"
+        fi
+
+        func_ls_vms
+        func_select_xml
+
+        # Set the absolute path to the VM XML file
+        domainxml="${vmdir}/${xmlname}"
+
+
+        func_get_tags
+        func_select_tag
+    fi
+
+    printf "\nSession Type:\t${get_session}"
+    printf "\nXML Filename:\t${domainxml}"
+    printf "\n-----------------------\n"
+    printf "XML Tag Info:\n"
+    finaltag=$(echo ${tagname} | awk -F'[<>]' '{print $2}' | awk '{print $1}')
+    echo "${tagname}" | command grep -E "\/" &>/dev/null
+    if [[ "$?" == "0" ]]; then
+        printf "Single Line Output for tag: ${finaltag}\n"
+        grep "${tagname}" "${domainxml}"
+    else
+        printf "Multi Line Output for tag: ${finaltag}\n"
+        sed -n "/<${finaltag}.*>/,/<\/${finaltag}.*>/p" "${domainxml}"
+    fi
+    printf "\n\n"
+}
+
+domainxml=("$@")
+main
