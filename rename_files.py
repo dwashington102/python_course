fe90f10febd62f14ae4f77b7245abf141f7c07ff
dwashington102 <dwashington102@gmail.com>
2023-01-21 07:41:16 -0600

    Changed to allow user to select ext. type

--- rename_files.py
+++ rename_files.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
 """
+Script allows user to input extension type (old), select new extension type,
+and rename all files in a directory using the old extension type to
+use the new extension type. 
+
+
+Exit Codes:
+101 - getdir(): Directory not found
+102 - getdir(): Permission denied when attempting to cd to directory
+103 - get_platform(): Attempt to run script on non-Linux device
+120 - getfiles(): cwd is empty
+
 """
 
+import argparse
 import os
 import re
 import sys
 
 
 def main():
+    get_platform()
     try:
+        userdir = os.getcwd()
+        print(f"Current Directory: {userdir}")
+
         defaultdir = currentdir()
         if defaultdir == "n":
             userdir = getdir()
-        else:
-            userdir = defaultdir
-            listfiles = getfiles(userdir)
-            searchpattern = getext()
-            do_work(listfiles, searchpattern, userdir)
+
+        listfiles = getfiles(userdir)
+        searchpattern = getext()
+        newext = getnewext()
+        do_work(listfiles, searchpattern, userdir, newext)
     except KeyboardInterrupt as ke:
         print("\nUser Interrupt received...exit(0)")
         sys.exit(0)
 
 
+def get_platform():
+    linuxplat = sys.platform
+    if linuxplat != 'linux':
+        print("\nScript must be ran on Linux device...exit(103)")
+        sys.exit(103)
+
 def currentdir():
-    mydir = os.getcwd()
-    print(f"Current Directory: {mydir}")
     defaultdir = input("Use current directory (y/n): ")
     return(defaultdir)
 
@@ -55,48 +75,72 @@ def getfiles(userdir):
     """
     Build a list that contains the files in the directory
     Exit Codes:
-        20 - The directory is empty
+        120 - The directory is empty
     """
     print("\n")
     listfiles = os.listdir()
     if len(listfiles) <= 0:
         print(f"Directory {userdir} is empty")
-        sys.exit(20)
+        sys.exit(120)
     return (listfiles)
 
 
 def getext():
     """
     Get file extension from user
-    Exit Codes:
 
     """
-    userext = str(input("Input File Ext.: "))
+    print("Input an extension type for all files to be renamed")
+    userext = str(input("Extension type: "))
     return (userext)
 
 
-def do_work(listfiles, searchpattern, userdir):
+def getnewext():
+    """
+    Get the new file extension
+    """
+    print("Input the new extension type:")
+    newext = str(input("New Extension type: "))
+    return (newext)
+    
+
+def do_work(listfiles, searchpattern, userdir, newext):
+    """
+    Function sets count=0, variable will be used to create new file name
+    Gathers a list (listfiles) of files in currenct directory
+    Loops over listfiles--> each item in list searched for file ext. user
+    entered
+    If ext is found, file is renamed using format:
+    {count variable}.{user entered ext}
+    """
+    print('\n')
     count = 0
-    orderlist = listfiles.sort(key=lambda f: int(''. join(filter(str. isdigit, f))))
-    listfiles.sort(key=lambda f: int(''. join(filter(str. isdigit, f))))
+    # If all filenames in the list begin with a digit lamba would be useful
+    # ---> see working_with-sort.py
+    #orderlist = listfiles.sort(key=lambda f: int(''. join(filter(str. isdigit, f))))
+    #listfiles.sort(key=lambda f: int(''. join(filter(str. isdigit, f))))
     for afile in listfiles:
         ftype = str(os.path.isfile(afile))
         if ftype == 'True':
-            if searchpattern in afile:
+            if afile.endswith(searchpattern):
                 count += 1
-                # print(f"DEBUG >>> filename: {afile} count: {count}")
-                newname = str(count) + "." + searchpattern
+                oldnametup = os.path.splitext(afile)
+                oldname = oldnametup[0]
+                #newname = str(count) + "." + searchpattern
+                newname = oldname + "." + newext
                 print(f"RENAME {afile} to {newname}")
                 try:
                     os.rename(afile, newname)
                 except PermissionError as eperm:
                     print(f"FAILED -- {eperm} for file {userdir}")
         else:
-            print(f"SKIPPING DIR {afile}")
+            print(f"SKIPPING directory: {afile}")
 
     if count == 0:
-        print(f"No files with ext. {searchpattern}"
-              "found in the directory {userdir}")
+        print(f"\nNo files with ext. {searchpattern}"
+              f" found in the directory {userdir}")
+    else:
+        print(f"\nTotal Files renamed: {count}")
 
 
 if __name__ == '__main__':
