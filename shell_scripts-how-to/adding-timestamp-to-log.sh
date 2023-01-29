d31f7530aa63bbe027577e3edae94210a5a15731
dwashington102 <dwashington102@gmail.com>
2023-01-28 21:09:09 -0600

    updating

--- shell_scripts-how-to/adding-timestamp-to-log.sh
+++ shell_scripts-how-to/adding-timestamp-to-log.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 #
-#
+:<<'COMMENTS'
+Script generates a log /tmp/bf_linuxatibm/{script}_{timestamp}.log
 
-logtimestamp (){
-# Function prints timestamp before the log entries
-command echo -n $(date --rfc-3339=seconds)" "
+Contents of the log file
+2023-01-28 20:59:26-06:00 write_log
+2023-01-28 20:59:28-06:00 action (1/3)
+2023-01-28 20:59:33-06:00 action (2/3)
+2023-01-28 20:59:33-06:00 action (3) failed
+2023-01-28 20:59:33-06:00 action (3/3)
+COMMENTS
+
+logtimestamp ()
+{
+    # Function prints timestamp before the log entries
+    command echo -n $(date --rfc-3339=seconds)" "
 }
 
 
-func_set_log (){
+func_set_log ()
+{
 # function to generate log file
     bf_temp_dir="/tmp/bf_linuxatibm"
     timeStamp=$(date +%Y%m%d_%H%M%S)
@@ -30,28 +41,28 @@ func_set_log (){
 
 
 
-write_log (){
-        # write function name to log file
-        logtimestamp ; echo "${FUNCNAME[0]}"
-        sleep 2
-        logtimestamp ; echo "action (1/3)"
-        sleep 5
-        logtimestamp ; echo "action (2/3)"
-        if  ! /usr/bin/rpm --query --quiet DEBUG &>/dev/null  ; then
-                logtimestamp ; echo "action (3) failed"
-         fi
-         logtimestamp ; echo "action (3/3)"
+write_log ()
+{
+    # write function name to log file
+    logtimestamp ; echo "${FUNCNAME[0]}"
+    sleep 2
+    logtimestamp ; echo "action (1/3)"
+    sleep 5
+    logtimestamp ; echo "action (2/3)"
+    if  ! /usr/bin/rpm --query --quiet DEBUG &>/dev/null  ; then
+        logtimestamp ; echo "action (3) failed"
+    fi
+    logtimestamp ; echo "action (3/3)"
 }
 
 
-MAIN (){
+MAIN ()
+{
     logtimestamp &>/dev/null
     func_set_log
     (
-    write_log
-) >> $logfile
+        write_log
+    ) >> $logfile
 }
 
 MAIN
-
-
