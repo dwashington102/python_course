e1ab0689575a89bb9be7d6dcc16a75c794403f28
dwashington102 <dwashington102@gmail.com>
2024-04-10 12:09:45 -0500

    initial push

--- python_scripts-how-to/useful/gather_list_of_os_users.py
+++ python_scripts-how-to/useful/gather_list_of_os_users.py
@@ -9,5 +9,8 @@ import pwd
 for osuser in pwd.getpwall():
     if osuser.pw_uid >= 1000 and osuser.pw_uid <= 59999:
         print(f"username: {osuser.pw_name}")
-        print(f"Userid for {osuser.pw_name} is {osuser.pw_uid}")
-
+        print(f"\tUID: {osuser.pw_uid}")
+        print(f"\tGID: {osuser.pw_gid}")
+        print(f"\tComment: {osuser.pw_gecos}")
+        print(f"\tHome: {osuser.pw_dir}")
+        print(f"\tShell: {osuser.pw_shell}")
