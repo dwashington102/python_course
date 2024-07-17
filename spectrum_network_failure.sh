c43d8a096d67e3388d0738eedb817dc8431e519a
dwashington102 <dwashington102@gmail.com>
2024-07-17 13:37:16 -0500

    updated to include new wifi name

--- spectrum_network_failure.sh
+++ spectrum_network_failure.sh
@@ -36,7 +36,7 @@ func_check_logdir (){
 
 
 func_check_spectrum (){
-    nmcli con | command grep Spectrum &>/dev/null
+    nmcli con | command grep ${NETNAME} &>/dev/null
     if [[ "$?" != "0" ]]; then
         printf "Not connected to Spectrum Network..exit 101\n"
         exit 101
@@ -64,7 +64,7 @@ do_work (){
                 touch ${logfile}
             fi
             lostcount=$((lostcount+1))
-            constate=$(nmcli -t con show TequilaToday-AF | command grep "GENERAL.STATE")
+            constate=$(nmcli -t con show $NETNAME | command grep "GENERAL.STATE")
             printf "Connection #${lostcount} failed at $(date +%Y%m%d_%H%M%S)\tSTATE:${constate}\n"
             restoreconrc=1
         else
@@ -113,26 +113,13 @@ main (){
             touch $logfile
     fi
 
+    NETNAME="TequilaToday-AF"
+
     (
     func_check_spectrum
     do_work
     ) >> ${logfile}
 }
-
-#if [[ ! -z "$1" ]]; then
-#    if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-#        printf "Usage and Help:\n"
-#        printf "Checks the device is connected to the network\n"
-#        printf "If the network is down, script write to the log and\n"
-#        printf "attempts to restart IBM VPN if it exists"
-#    else
-#        printf "spectrum_network_failure: Invalid option --- '$1'\n"
-#        printf "Try 'spectrum_network_failure.sh --help'\n"
-#    fi
-#    printf "\n"
-#    exit 0
-#fi
-
 
 
 main $@
